1acd0b4940abfc1726ba899e2b2f91b914a5c5e4
Joaquin <pelle.joaquin@gmail.com>
2022-09-30 13:00:45 -0400

    Add benchmark test of OTE initial data

---
+++ test/benchmarks/initialdataOTE_benchmark.jl
@@ -0,0 +1,36 @@
+using Skylight, BenchmarkTools, BenchmarkPlots, StatsPlots
+
+suite = BenchmarkGroup()
+
+spacetime = KerrSpacetimeKerrSchildCoordinates(parameters = KerrSpacetimeParameters(M=1.0,
+                                                                                a=0.9))
+
+numbers = [100,200,400,800]
+
+for n in numbers
+
+    image_plane = ImagePlane(observer_distance = 500.0,
+                        observer_inclination_in_degrees = 90,
+                        horizontal_side_image_plane = 10.0,
+                        vertical_side_image_plane = 10.0,
+                        horizontal_number_of_nodes = n,
+                        vertical_number_of_nodes = n)
+
+    configurations = OTEInitialDataConfigurations(spacetime=spacetime,
+                                            image_plane = image_plane,
+                                            initial_times = [0.0])
+
+    suite[string(n)] = @benchmarkable initialize_OTE($configurations)
+    
+end
+
+tune!(suite)
+t = run(suite)
+
+p = plot()
+
+for n in numbers
+    plot!(p, t[string(n)],yaxis=:log10)
+end
+
+plot(p)
