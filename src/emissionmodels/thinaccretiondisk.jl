94056cf87c9f3e6f3f09c7d2b77e5ac09dec1d5f
Joaquin <pelle.joaquin@gmail.com>
2022-10-09 17:58:40 -0400

    Add thin accretion disk differential

--- src/emissionmodels/thinaccretiondisk.jl
+++ src/emissionmodels/thinaccretiondisk.jl
@@ -1,1 +1,22 @@
 struct ThinAccretionDisk <: SurfaceEmissionModel end
+
+function surface_function(position, model::ThinAccretionDisk, coord_system::CartesianKind)
+    
+    @views z = position[4]
+    
+    return z 
+
+end
+
+function set_surface_differential!(covector, position, model::ThinAccretionDisk, coord_system::CartesianKind)
+
+    @views begin
+        t,x,y,z = position
+    end
+
+    covector[1] = 0.0
+    covector[2] = 0.0
+    covector[3] = 0.0
+    covector[4] = 1.0
+
+end
