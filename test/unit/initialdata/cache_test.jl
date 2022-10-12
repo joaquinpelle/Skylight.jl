833d80459fdd3a7a47e3af14677a853efe49ece4
Joaquin <pelle.joaquin@gmail.com>
2022-10-11 21:08:48 -0400

    Lower precision of approx test

--- test/unit/initialdata/cache_test.jl
+++ test/unit/initialdata/cache_test.jl
@@ -77,15 +77,15 @@ using Skylight, Test
                 @test cache.metric == [-1.0 0.0 0.0 0.0; 0.0 1.0 0.0 0.0; 0.0 0.0 1.0 0.0; 0.0 0.0 0.0 1.0]
                 @test cache.metric_inverse == [-1.0 0.0 0.0 0.0; 0.0 1.0 0.0 0.0; 0.0 0.0 1.0 0.0; 0.0 0.0 0.0 1.0]
                 @test tetrad[:,1] ≈ [1.0, 0.0, 0.0, 0.0]
-                @test Skylight.scalar_product(tetrad[:,1], tetrad[:,2], cache.metric) ≈ 0.0 atol = 1e-14
-                @test Skylight.scalar_product(tetrad[:,1], tetrad[:,3], cache.metric) ≈ 0.0 atol = 1e-14
-                @test Skylight.scalar_product(tetrad[:,1], tetrad[:,4], cache.metric) ≈ 0.0 atol = 1e-14
-                @test Skylight.scalar_product(tetrad[:,2], tetrad[:,2], cache.metric) ≈ 1.0 atol = 1e-14 
-                @test Skylight.scalar_product(tetrad[:,2], tetrad[:,3], cache.metric) ≈ 0.0 atol = 1e-14 
-                @test Skylight.scalar_product(tetrad[:,2], tetrad[:,4], cache.metric) ≈ 0.0 atol = 1e-14
-                @test Skylight.scalar_product(tetrad[:,3], tetrad[:,3], cache.metric) ≈ 1.0 atol = 1e-14
-                @test Skylight.scalar_product(tetrad[:,3], tetrad[:,4], cache.metric) ≈ 0.0 atol = 1e-14
-                @test Skylight.scalar_product(tetrad[:,4], tetrad[:,4], cache.metric) ≈ 1.0 atol = 1e-14
+                @test Skylight.scalar_product(tetrad[:,1], tetrad[:,2], cache.metric) ≈ 0.0 atol = 1e-13
+                @test Skylight.scalar_product(tetrad[:,1], tetrad[:,3], cache.metric) ≈ 0.0 atol = 1e-13
+                @test Skylight.scalar_product(tetrad[:,1], tetrad[:,4], cache.metric) ≈ 0.0 atol = 1e-13
+                @test Skylight.scalar_product(tetrad[:,2], tetrad[:,2], cache.metric) ≈ 1.0 atol = 1e-13 
+                @test Skylight.scalar_product(tetrad[:,2], tetrad[:,3], cache.metric) ≈ 0.0 atol = 1e-13 
+                @test Skylight.scalar_product(tetrad[:,2], tetrad[:,4], cache.metric) ≈ 0.0 atol = 1e-13
+                @test Skylight.scalar_product(tetrad[:,3], tetrad[:,3], cache.metric) ≈ 1.0 atol = 1e-13
+                @test Skylight.scalar_product(tetrad[:,3], tetrad[:,4], cache.metric) ≈ 0.0 atol = 1e-13
+                @test Skylight.scalar_product(tetrad[:,4], tetrad[:,4], cache.metric) ≈ 1.0 atol = 1e-13
             
             end
             
@@ -115,11 +115,11 @@ using Skylight, Test
                 @test cache.metric_inverse == [-1.0 0.0 0.0 0.0; 0.0 1.0 0.0 0.0; 0.0 0.0 1.0 0.0; 0.0 0.0 0.0 1.0]
                 @test tetrad[:,1] ≈ [1.0/sqrt(0.9775), 0.0, 0.15/sqrt(0.9775), 0.0]
                 @test tetrad[:,2] ≈ [0.0, 0.6, 0.0, 0.8]
-                @test Skylight.scalar_product(tetrad[:,2], tetrad[:,3], cache.metric) ≈ 0.0 atol = 1e-14 
-                @test Skylight.scalar_product(tetrad[:,2], tetrad[:,4], cache.metric) ≈ 0.0 atol = 1e-14
-                @test Skylight.scalar_product(tetrad[:,3], tetrad[:,3], cache.metric) ≈ 1.0 atol = 1e-14
-                @test Skylight.scalar_product(tetrad[:,3], tetrad[:,4], cache.metric) ≈ 0.0 atol = 1e-14
-                @test Skylight.scalar_product(tetrad[:,4], tetrad[:,4], cache.metric) ≈ 1.0 atol = 1e-14
+                @test Skylight.scalar_product(tetrad[:,2], tetrad[:,3], cache.metric) ≈ 0.0 atol = 1e-13 
+                @test Skylight.scalar_product(tetrad[:,2], tetrad[:,4], cache.metric) ≈ 0.0 atol = 1e-13
+                @test Skylight.scalar_product(tetrad[:,3], tetrad[:,3], cache.metric) ≈ 1.0 atol = 1e-13
+                @test Skylight.scalar_product(tetrad[:,3], tetrad[:,4], cache.metric) ≈ 0.0 atol = 1e-13
+                @test Skylight.scalar_product(tetrad[:,4], tetrad[:,4], cache.metric) ≈ 1.0 atol = 1e-13
             
             end
         end
