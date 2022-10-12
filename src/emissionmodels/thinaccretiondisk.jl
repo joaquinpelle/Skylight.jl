9306cc267f5cf9e5ad36e0b8c09c9c7557a63dd5
Joaquin <pelle.joaquin@gmail.com>
2022-10-12 18:31:57 -0400

    Differential of accrretion disk model

--- src/emissionmodels/thinaccretiondisk.jl
+++ src/emissionmodels/thinaccretiondisk.jl
@@ -1,11 +1,13 @@
-struct ThinAccretionDisk <: SurfaceEmissionModel end
+struct ThinAccretionDisk{T::Function} <: SurfaceEmissionModel end
 
 function surface_function(position, model::ThinAccretionDisk, coord_system::CartesianKind)
-    
     @views z = position[4]
-    
     return z 
+end
 
+function surface_function(position, model::ThinAccretionDisk, coord_system::SphericalKind)
+    @views θ = position[3]
+    return θ-π/2 
 end
 
 function set_surface_differential!(covector, position, model::ThinAccretionDisk, coord_system::CartesianKind)
@@ -20,3 +22,17 @@ function set_surface_differential!(covector, position, model::ThinAccretionDisk,
     covector[4] = 1.0
 
 end
+
+function set_surface_differential!(covector, position, model::ThinAccretionDisk, coord_system::SphericalKind)
+
+    @views begin
+        t,r,θ,φ = position
+    end
+
+    covector[1] = 0.0
+    covector[2] = 0.0
+    covector[3] = 1.0
+    covector[4] = 0.0
+
+end    
+
