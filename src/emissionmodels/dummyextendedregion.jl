d8e5d69398a520e6f59f04109b0797d017056015
Joaquin <pelle.joaquin@gmail.com>
2022-10-11 20:56:46 -0400

    Add dummy model functions

--- src/emissionmodels/dummyextendedregion.jl
+++ src/emissionmodels/dummyextendedregion.jl
@@ -1,1 +1,7 @@
 struct DummyExtendedRegion <: EmissionModel end
+
+function set_model_four_velocity!(vector, position, metric, model::DummyExtendedRegion, coord_system)
+    vector .= ∂t()
+end
+
+get_number_of_points(model::DummyExtendedRegion) = 3
