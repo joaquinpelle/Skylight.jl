77cfc2162dc5b4ebaa8b410118c327cae9bba10a
Joaquin <pelle.joaquin@gmail.com>
2022-10-11 22:12:16 -0400

    Remove @time from tests since now its inbuilt

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,15 +2,15 @@ using Skylight, Test, SafeTestsets
 
 @time begin
 
-    @time @safetestset "Geometry" begin include("unit/utils/geometry_test.jl") end
-    @time @safetestset "Random points" begin include("unit/utils/randompoints_test.jl") end
-    @time @safetestset "Tetrads" begin include("unit/utils/tetrads_test.jl") end
-    @time @safetestset "Emission models" begin include("unit/emissionmodels_test.jl") end
-    @time @safetestset "Spacetimes" begin include("unit/spacetimes_test.jl") end
-    @time @safetestset "Initial data cache" begin include("unit/initialdata/cache_test.jl") end
-    @time @safetestset "Image plane" begin include("unit/initialdata/imageplane_test.jl") end
-    @time @safetestset "Pixel position and momentum" begin include("unit/initialdata/pixel_position_momentum_test.jl") end
-    @time @safetestset "Observer to emitter" begin include("unit/initialdata/observertoemitter_test.jl") end
-    @time @safetestset "Emitter to observer" begin include("unit/initialdata/emittertoobserver_test.jl") end
+    @safetestset "Geometry" begin include("unit/utils/geometry_test.jl") end
+    @safetestset "Random points" begin include("unit/utils/randompoints_test.jl") end
+    @safetestset "Tetrads" begin include("unit/utils/tetrads_test.jl") end
+    @safetestset "Emission models" begin include("unit/emissionmodels_test.jl") end
+    @safetestset "Spacetimes" begin include("unit/spacetimes_test.jl") end
+    @safetestset "Initial data cache" begin include("unit/initialdata/cache_test.jl") end
+    @safetestset "Image plane" begin include("unit/initialdata/imageplane_test.jl") end
+    @safetestset "Pixel position and momentum" begin include("unit/initialdata/pixel_position_momentum_test.jl") end
+    @safetestset "Observer to emitter" begin include("unit/initialdata/observertoemitter_test.jl") end
+    @safetestset "Emitter to observer" begin include("unit/initialdata/emittertoobserver_test.jl") end
 
 end
