a168c088d80f6763c1833a928906ed066ad59492
Joaquin <pelle.joaquin@gmail.com>
2022-10-21 18:58:20 -0400

    Add tentative output type management

--- src/geodesics/configurations.jl
+++ src/geodesics/configurations.jl
@@ -1,11 +1,20 @@
 export GeodesicsConfigurations
 
-@with_kw struct GeodesicsConfigurations{T<:InitialDataConfigurations,M}
+abstract type OutputType end
+
+struct SaveGeodesic <: OutputType end
+struct SaveEndpoint <: OutputType end
+
+output_func(output_type::SaveGeodesics) = (sol, i) -> (sol, false)
+output_func(output_type::SaveEndpoint) = (sol, i) -> (sol[end], false)
+
+@with_kw struct GeodesicsConfigurations{T<:InitialDataConfigurations, O<:IntegrationOutput, M}
     
-    initial_data_configs::T
-    abstol::Float64
-    reltol::Float64
-    method::M
     Rmax::Float64
+    output::O = SaveEndpoint()
+    abstol::Float64 = 1e-21
+    reltol::Float64 = 1e-13
+    method::M = VCABM()
 
 end
+
