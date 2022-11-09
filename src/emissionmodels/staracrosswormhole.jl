063a48b0e863cf2d5736bfa66cc9384c03e2aa8e
Joaquin <pelle.joaquin@gmail.com>
2022-11-09 18:45:41 -0500

    Rename and update star-wormhole model file

---
+++ src/emissionmodels/staracrosswormhole.jl
@@ -0,0 +1,8 @@
+export StarAcrossWormhole
+
+@with_kw struct StarAcrossWormhole <: OpaqueInteriorSurfaceEmissionModel 
+
+    l_center::Float64
+    star_radius::Float64
+
+end
--- src/emissionmodels/starbehindwormhole.jl
+++
@@ -1,1 +0,0 @@
-struct StarBehindWormhole <: OpaqueInteriorSurfaceEmissionModel end
