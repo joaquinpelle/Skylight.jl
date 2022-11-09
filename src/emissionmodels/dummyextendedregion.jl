2d65b47f822a2bafb0b451aea28e304d334d0d2a
Joaquin <pelle.joaquin@gmail.com>
2022-11-09 18:46:28 -0500

    Add dummy coordinate system to dummy model

--- src/emissionmodels/dummyextendedregion.jl
+++ src/emissionmodels/dummyextendedregion.jl
@@ -1,5 +1,7 @@
 struct DummyExtendedRegion <: EmissionModel end
 
+coordinate_system_class(DummyExtendedRegion) = CartesianClass()
+
 function set_model_four_velocity!(vector, position, metric, model::DummyExtendedRegion, coord_system)
     vector .= ∂t()
 end
