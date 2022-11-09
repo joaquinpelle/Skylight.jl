d7d1694d2a2cd4673064ddd278fa762e4da1e8b7
Joaquin <pelle.joaquin@gmail.com>
2022-11-09 18:44:17 -0500

    Rename and update run scripts

---
+++ run/runETO.jl
@@ -0,0 +1,22 @@
+using Skylight
+
+spacetime = KerrSpacetimeKerrSchildCoordinates(M=1.0,a=0.9)
+
+model = SyntheticPolarCap(number_of_points=10, 
+                          star_radius=5.0,
+                          angular_speed = 0.05, 
+                          misalignment_angle_in_degrees=90,
+                          angular_radius_in_degrees=60, 
+                          temperature=1.0)
+
+configurations = ETOConfigurations(spacetime=spacetime,
+                                   emission_model = model,
+                                   number_of_packets_per_point = 100, observer_distance = 500.0)
+
+initial_data = get_initial_data(configurations)
+
+cb, cb_params = get_callback_and_params(configurations) #... or, define your own cb and cb_params
+
+solver_options = SolverOptions(method=VCABM(), reltol=1e-13, abstol=1e-21, output_type = SaveEndpoint())
+
+output_data = integrate_geodesics(initial_data, configurations, cb, cb_params, solver_options)
---
+++ run/runOTE.jl
@@ -0,0 +1,30 @@
+using Skylight
+
+spacetime = KerrSpacetimeKerrSchildCoordinates(M=1.0,a=0.9)
+
+image_plane = ImagePlane(observer_distance = 500.0,
+                         observer_inclination_in_degrees = 45,
+                         horizontal_side_image_plane = 10.0,
+                         vertical_side_image_plane = 10.0,
+                         horizontal_number_of_nodes = 50,
+                         vertical_number_of_nodes = 50)
+
+model = SyntheticPolarCap(number_of_points=10, 
+                          star_radius=5.0,
+                          angular_speed = 0.05, 
+                          misalignment_angle_in_degrees=90,
+                          angular_radius_in_degrees=60, 
+                          temperature=1.0)
+        
+configurations = OTEConfigurations(spacetime=spacetime,
+                                   image_plane = image_plane,
+                                   initial_times = [0.0,1.0],
+                                   emission_model = model)
+
+initial_data = get_initial_data(configurations)
+
+cb, cb_params = get_callback_and_params(configurations) #... or, define your own cb and cb_params
+
+solver_options = SolverOptions(method=VCABM(), reltol=1e-13, abstol=1e-21, output_type = SaveEndpoint())
+
+output_data = integrate_geodesics(initial_data, configurations, cb, cb_params, solver_options)
