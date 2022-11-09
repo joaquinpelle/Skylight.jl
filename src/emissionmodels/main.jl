5dacd85c0c0cc9b8168b0618061079d0aa73de3f
Joaquin <pelle.joaquin@gmail.com>
2022-11-09 18:47:15 -0500

    Organize models in groups

--- src/emissionmodels/main.jl
+++ src/emissionmodels/main.jl
@@ -4,19 +4,14 @@ abstract type SurfaceEmissionModel <: EmissionModel end
 
 abstract type OpaqueInteriorSurfaceEmissionModel <: SurfaceEmissionModel end
 
+abstract type NeutronStarHotSpots <: OpaqueInteriorSurfaceEmissionModel end
+abstract type BlackHoleAccretionDisk <: SurfaceEmissionModel end
+abstract type BlackHoleCorona <: SurfaceEmissionModel end
+
 include("syntheticpolarcap.jl")
 include("onionhotspots.jl")
 include("bogdanovpolarcap.jl")
-include("thinaccretiondisk.jl")
-include("blackholecorona.jl")
-include("starbehindwormhole.jl")
+include("blackholeaccretiondisk.jl")
+include("staracrosswormhole.jl")
 include("dummyextendedregion.jl")
-
-
-function set_unit_surface_normal!(vector, position, metric, metric_inverse, model, coord_system)
-
-    set_surface_differential!(vector, position, model, coord_system)
-    vector .= raise_index(vector,metric_inverse)
-    normalize_spacelike!(vector, metric)
-
-end
+include("utils.jl")
