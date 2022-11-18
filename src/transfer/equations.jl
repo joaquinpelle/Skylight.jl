398964366b5361d38aa337116c5779d515298a42
Joaquin <pelle.joaquin@gmail.com>
2022-11-17 19:25:31 -0500

    Refactor equations and add generic assignment

--- src/transfer/equations.jl
+++ src/transfer/equations.jl
@@ -1,4 +1,14 @@
-function geodesic_equations!(du, u::Array{Float64,1}, p::GeodesicsCache, t)
+equations(configurations::VacuumConfigurations) = geodesic_equations!
+equations(configurations::NonVacuumConfigurations) = non_vacuum_equations!
+
+function non_vacuum_equations!(du, u::Array{Float64,1}, p::NonVacuumCache, t)
+
+    geodesic_equations!(du, u, p, t)
+    transfer_equations!(du, u, p, t)
+
+end
+
+function geodesic_equations!(du, u::Array{Float64,1}, p, t)
 
     spacetime = p.spacetime
     cache = p.multi_thread[Threads.threadid()]
@@ -35,43 +45,7 @@ function geodesic_equations!(du, u::Array{Float64,1}, p::GeodesicsCache, t)
 
 end
 
-
-function transfer_equations!(du, u::Array{Float64,1}, p::TransferCache, t)
-
-    spacetime = p.spacetime
-    cache = p.multi_thread[Threads.threadid()]
-
-    point = cache.point
-    vel = cache.velocity
-
-    for i in 1:4
-        point[i] = u[i]
-        vel[i] = u[4+i]
-    end
-
-    Γ = cache.christoffel
-    fill!(Γ, 0.0)
-
-    christoffel_cache = cache.christoffel_cache
-    set_christoffel!(Γ, point, spacetime, christoffel_cache) 
-
-    a = cache.acceleration
-    fill!(a, 0.0)
-
-    for i in 1:4
-        for j in 1:4
-            for k in 1:4
-                a[i] += -Γ[i,j,k]*vel[j]*vel[k]
-            end
-        end
-    end
-
-    for i in 1:4
-        du[i] = vel[i]
-        du[4+i] = a[i]
-    end
-
-    #Set the transfer variables
+function transfer_equations!(du, u::Array{Float64,1}, p, t)
 
     model = p.model
     observed_energies = p.observed_energies
