fa2b4d26a1fe27a1dbfcfda9340dcc955df5b15e
Joaquin <pelle.joaquin@gmail.com>
2023-04-09 13:30:05 -0300

    Make interpolation kind a keyword argument

--- src/utils/interpolation.jl
+++ src/utils/interpolation.jl
@@ -1,12 +1,12 @@
-function my_interpolation(ydata, xdata, kind)
+function my_interpolation(ydata, xdata; kind=nothing)
     
     if kind == "linear"
         return LinearInterpolation(ydata,xdata)
-    elseif kind == "cubic" 
+    elseif kind == "cubicspline" 
         return CubicSpline(ydata,xdata)
     elseif kind == "loglinear"
         return LinearInterpolation(log10.(ydata),log10.(xdata))
-    elseif kind == "logcubic" 
+    elseif kind == "logcubicspline" 
         return CubicSpline(log10.(ydata),log10.(xdata))
     else error("Kind not defined.")        
     end
@@ -14,5 +14,5 @@ function my_interpolation(ydata, xdata, kind)
 end
 
 function type_of_interpolator(kind)
-    typeof(my_interpolation(ones(2),[0.0, 1.0],kind))
+    typeof(my_interpolation(ones(2),[0.0, 1.0]; kind=kind))
 end
