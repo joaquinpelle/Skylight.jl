8c14818112990e00e5be61a73d2ffc8d1edf5945
Joaquin <pelle.joaquin@gmail.com>
2023-04-17 12:36:50 -0300

    Change unit conversion functions signature calls

--- src/utils/units.jl
+++ src/utils/units.jl
@@ -1,11 +1,11 @@
 export geometrized_to_CGS
 export CGS_to_geometrized
 
-geometrized_to_CGS(magnitude; dim, M1::Number) = magnitude*geometrized_unit_magnitude_in_CGS(dim; M1=M1) 
-CGS_to_geometrized(magnitude; dim, M1::Number) = magnitude/geometrized_unit_magnitude_in_CGS(dim; M1=M1)
+geometrized_to_CGS(magnitude, dim; M1::Number) = magnitude*geometrized_unit_magnitude_in_CGS(dim; M1=M1) 
+CGS_to_geometrized(magnitude, dim; M1::Number) = magnitude/geometrized_unit_magnitude_in_CGS(dim; M1=M1)
 
-geometrized_to_CGS(magnitude; dim, configurations::Configurations) = magnitude*geometrized_unit_magnitude_in_CGS(dim; M1=configurations.M1) 
-CGS_to_geometrized(magnitude; dim, configurations::Configurations) = magnitude/geometrized_unit_magnitude_in_CGS(dim; M1=configurations.M1)
+geometrized_to_CGS(magnitude, dim, configurations::Configurations) = magnitude*geometrized_unit_magnitude_in_CGS(dim; M1=configurations.unit_mass_in_solar_masses)
+CGS_to_geometrized(magnitude, dim, configurations::Configurations) = magnitude/geometrized_unit_magnitude_in_CGS(dim; M1=configurations.unit_mass_in_solar_masses)
 
 function geometrized_unit_magnitude_in_CGS(dim; M1)
 
@@ -18,7 +18,7 @@ function geometrized_unit_magnitude_in_CGS(dim; M1)
 
     M1_cgs = M1*M_sun        #Geometrized unit mass in CGS 
     L1_cgs = G*M1_cgs/c^2    #Geometrized unit length in CGS
-    T1_cgs = L1_CGS/c        #Geometrized unit time in CGS
+    T1_cgs = L1_cgs/c        #Geometrized unit time in CGS
 
     return L1_cgs^dim.L*M1_cgs^dim.M*T1_cgs^dim.T
 
