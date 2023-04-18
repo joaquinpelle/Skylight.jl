291657831bdc331046f4b2e0029ce98c312fefc7
Joaquin <pelle.joaquin@gmail.com>
2023-04-17 22:25:51 -0300

    Fix call signature of intensity grid

--- run/rundummy.jl
+++ run/rundummy.jl
@@ -26,7 +26,7 @@ cb, cb_params = get_callback_and_params(configurations; rhorizon_bound = 0.3) #.
 output_data = integrate(initial_data, configurations, cb, cb_params; τmax=2.0, method=VCABM(), reltol=1e-13, abstol=1e-21)
 
 xs, ys = get_pixel_coordinates_vectors(configurations) 
-zs = view_intensities_grid(output_data, configurations, 1)
+zs = view_intensities_grid(output_data, configurations; E_idx = 1)
 
 fig = Figure(font = "CMU Serif") #resolution=(600,400)
 ax = Axis(fig[1,1], xlabel=L"\alpha", ylabel=L"\beta", ylabelsize = 26, xlabelsize = 26) 
