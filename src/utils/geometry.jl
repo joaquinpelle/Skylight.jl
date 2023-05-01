7e52d1a73a2d2e38b0da59e7cc0670cc3ee77497
Joaquin <pelle.joaquin@gmail.com>
2023-05-01 11:31:23 -0300

    Add cosine angle between vectors

--- src/utils/geometry.jl
+++ src/utils/geometry.jl
@@ -4,6 +4,28 @@ raise_index(v, metric_inverse) = metric_inverse*v
 scalar_product(v,u,metric) = dot(v,metric,u)
 norm_squared(v,metric) = scalar_product(v,v,metric)
 
+"""Projection of v orthogonal to normalized timelike vector u"""
+orthogonal_projection(v, u, metric) = v + u*scalar_product(v,u,metric) 
+
+"""The cosine of the angle between vectors v and w as seen by observer at normalized four-velocity"""
+
+function cos_angle_between_vectors(v, w, u, metric)
+
+
+    vp = orthogonal_projection(v, u, metric)  
+    wp = orthogonal_projection(w, u, metric)
+
+    vp_wp = scalar_product(vp, wp, metric)
+    vp2 = scalar_product(vp, vp, metric)
+    wp2 = scalar_product(wp, wp, metric)
+
+    return vp_wp/sqrt(vp2*wp2)
+
+end
+
+"""The cosine of the angle between null vectors v and w as seen by observer at normalized four-velocity u"""
+cos_angle_between_null_vectors(v, w, u, metric) = 1.0+vector_scalar_product(v, w, metric)/(vector_scalar_product(v, u, metric)*vector_scalar_product(w, u, metric))
+
 ∂t() = [1.0, 0.0, 0.0, 0.0]
 
 function normalize_timelike!(v, metric)
@@ -17,6 +39,7 @@ function normalize_spacelike!(v, metric)
     v ./= sqrt(norm_squared(v,metric))
 
 end
+
 
 function tangent_vector_zaxis_rotation!(vector, position, angular_speed, metric, coord_system::CartesianClass)
     
