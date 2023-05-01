a9ccf5c6baef48fe08a81647710254edc56bc8a7
Joaquin <pelle.joaquin@gmail.com>
2023-05-01 11:18:33 -0300

    Add spacetime informal interface

--- src/spacetimes/main.jl
+++ src/spacetimes/main.jl
@@ -14,6 +14,18 @@ struct SphericalClass <: CoordinateSystemClass end
 
 abstract type ChristoffelCache end
 
+#Required
+coordinate_system_class(spacetime::Spacetime) = error("Coordinate system not defined for this spacetime.")
+set_metric!(spacetime::Spacetime) = error("Metric not defined for this spacetime.")
+allocate_christoffel_cache(spacetime::Spacetime) = error("Christoffel cache not defined for this spacetime.")
+set_christoffel!(Γ, position, spacetime::Spacetime, cache) = error("Christoffel symbols not defined for this spacetime.")
+
+#Optional
+set_metric_inverse!(spacetime::Spacetime) = error("Metric inverse not defined for this spacetime.")
+event_horizon_radius(spacetime::Spacetime) = error("Event horizon radius not defined for this spacetime.")
+circular_geodesic_angular_speed(spacetime::Spacetime) = error("Circular geodesic angular speed not defined for this spacetime.")
+
+
 include("coordinate_alias.jl")
 include("minkowski.jl")
 include("schwarzschild.jl")
