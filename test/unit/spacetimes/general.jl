16d2fde21255a949011fe8005e451b2a53698063
Joaquin <pelle.joaquin@gmail.com>
2023-05-10 20:09:44 -0300

    Make test pass

--- test/unit/spacetimes/general.jl
+++ test/unit/spacetimes/general.jl
@@ -1,3 +1,5 @@
+using Parameters, PreallocationTools
+
 @testset "Automatic differentiation" begin
     
     M, a = 1.0, 0.5
@@ -10,20 +12,19 @@
 
     #Define a Kerr spacetime to use with automatic differentiation
 
-    @with_kw struct ADKerrSpacetimeKerrSchildCoordinates{T} <: BlackHoleSpacetime
+    struct ADKerrSpacetimeKerrSchildCoordinates
 
         M::Float64
         a::Float64
     
         @assert M >= 0.0
         @assert abs(a) <= M 
-    
-        #Metric cache
-        l::T = DiffCache(zeros(4))
-    
+        
+        ADKerrSpacetimeKerrSchildCoordinates(M,a) =  new(M,a)
+
     end
 
-    function set_metric!(g, position, spacetime::ADKerrSpacetimeKerrSchildCoordinates)
+    function Skylight.set_metric!(g, position, spacetime::ADKerrSpacetimeKerrSchildCoordinates)
 
         M = spacetime.M
         a = spacetime.a
@@ -35,34 +36,33 @@
         r = sqrt(r2)
         H2 = 2. * M * r / (r2 + a2 * z^2 / r2)
         
-        l = get_tmp(spacetime.l, position)
-        l[1] = 1.
-        l[2] = (r*x + a*y)/(r2 + a2)
-        l[3] = (r*y - a*x)/(r2 + a2)
-        l[4] = z/r
+        l1 = 1.
+        l2 = (r*x + a*y)/(r2 + a2)
+        l3 = (r*y - a*x)/(r2 + a2)
+        l4 = z/r
         
-        g[1,1]=-1. + H2 * l[1]*l[1]
-        g[1,2]= 0. + H2 * l[1]*l[2]
-        g[1,3]= 0. + H2 * l[1]*l[3]
-        g[1,4]= 0. + H2 * l[1]*l[4]
+        g[1,1]=-1. + H2 * l1*l1
+        g[1,2]= 0. + H2 * l1*l2
+        g[1,3]= 0. + H2 * l1*l3
+        g[1,4]= 0. + H2 * l1*l4
         g[2,1]= g[1,2]
-        g[2,2]= 1. + H2 * l[2]*l[2]
-        g[2,3]= 0. + H2 * l[2]*l[3]
-        g[2,4]= 0. + H2 * l[2]*l[4]
+        g[2,2]= 1. + H2 * l2*l2
+        g[2,3]= 0. + H2 * l2*l3
+        g[2,4]= 0. + H2 * l2*l4
         g[3,1]= g[1,3]
         g[3,2]= g[2,3]
-        g[3,3]= 1. + H2 * l[3]*l[3]
-        g[3,4]= 0. + H2 * l[3]*l[4]
+        g[3,3]= 1. + H2 * l3*l3
+        g[3,4]= 0. + H2 * l3*l4
         g[4,1]= g[1,4]
         g[4,2]= g[2,4]
         g[4,3]= g[3,4]
-        g[4,4]= 1. + H2 * l[4]*l[4]
+        g[4,4]= 1. + H2 * l4*l4
         
         return nothing
         
     end
     
-    spacetimeAD = ADKerrSpacetimeKerrSchildCoordinates(M=M, a=a)
+    spacetimeAD = ADKerrSpacetimeKerrSchildCoordinates(M, a)
     ΓAD = zeros(4,4,4) 
     #We didn't define set_christoffel! for this spacetime so it's going to use the default AutoDiff routines
     cacheAD = allocate_christoffel_cache(spacetimeAD)
