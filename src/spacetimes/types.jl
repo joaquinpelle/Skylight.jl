a2db39faa5fb436d77ac2aea273b085f63c88cb8
Joaquin <pelle.joaquin@gmail.com>
2023-05-26 16:32:28 -0300

    Add spacetime symemtries traits

--- src/spacetimes/types.jl
+++ src/spacetimes/types.jl
@@ -10,3 +10,21 @@ abstract type AbstractRotationSense end
 
 struct ProgradeRotation <: AbstractRotationSense end
 struct RetrogradeRotation <: AbstractRotationSense end
+
+abstract type AbstractSpacetimeSymmetry end
+
+abstract type Stationarity <: AbstractSpacetimeSymmetry end
+
+struct IsStationary <: Stationarity end 
+struct IsNotStationary <: Stationarity end 
+
+abstract type SphericalSymmetry <: AbstractSpacetimeSymmetry end
+
+struct IsSphericallySymmetric <: SphericalSymmetry end
+struct IsNotSphericallySymmetric <: SphericalSymmetry end
+
+abstract type AxialSymmetry <: AbstractSpacetimeSymmetry end
+
+struct IsAxiallySymmetric <: AxialSymmetry end
+struct IsNotAxiallySymmetric <: AxialSymmetry end
+
