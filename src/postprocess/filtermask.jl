ed84ca33588a8f23a22d33fa27b244ffb2004dff
Joaquin <pelle.joaquin@gmail.com>
2023-05-26 19:58:27 -0300

    Format some files

--- src/postprocess/filtermask.jl
+++ src/postprocess/filtermask.jl
@@ -2,33 +2,24 @@
 # that will filter out the rays which are away from the source
 
 function get_filter_mask(output_data, configurations::VacuumOTEConfigurations)
-
     spacetime = configurations.spacetime
     model = configurations.radiative_model
 
     Nrays = number_of_initial_conditions(configurations)
     filter_mask = zeros(Bool, Nrays)
-
     for i in 1:Nrays
-
         @views begin 
-
             position = output_data[1:4,i]
-
         end
 
         filter_mask[i] = is_final_position_at_source(position, spacetime, model)
-
     end
-
     return filter_mask
-
 end
 
 # Now the same for the ETO scheme
 
 function get_filter_mask(output_data, configurations::VacuumETOConfigurations)
-
     spacetime = configurations.spacetime
     model = configurations.radiative_model
 
@@ -36,17 +27,11 @@ function get_filter_mask(output_data, configurations::VacuumETOConfigurations)
     filter_mask = zeros(Bool, Nrays)
 
     for i in 1:Nrays
-
         @views begin 
-
             position = output_data[1:4,i]
-
         end
 
         filter_mask[i] = is_final_position_at_observer(position, spacetime, model)
-
     end
-
     return filter_mask
-
 end
