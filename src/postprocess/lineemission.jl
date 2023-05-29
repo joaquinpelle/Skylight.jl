df712e509361beb093d17dbb00e07c217d1e0a60
Joaquin <pelle.joaquin@gmail.com>
2023-05-29 01:24:10 -0300

    Exclude new line method for now

--- src/postprocess/lineemission.jl
+++ src/postprocess/lineemission.jl
@@ -9,9 +9,9 @@ Compute the binned intensity of a line emission spectrum.
 - `initial_data`: Initial condition data.
 - `output_data`: Output data from the radiation model.
 - `configurations::VacuumOTEConfigurations`: Configuration parameters for the model.
-- `emission_profile::Function`: User-defined function describing the emission profile.
 
 # Keywords
+- `emission_profile::Function`: User-defined function describing the emission profile.
 - `bin_size::Union{Nothing, Number}=nothing`: Size of each bin. Either `bin_size` or `num_bins` must be specified.
 - `num_bins::Union{Nothing, Number}=nothing`: Number of bins. Either `bin_size` or `num_bins` must be specified.
 - `start::Union{Nothing, Number}=nothing`: Lower bound of the range to be binned. If unspecified, the minimum of the energy quotients will be used.
@@ -25,8 +25,8 @@ function line_emission_spectrum(
     output_data, 
     configurations::VacuumOTEConfigurations; 
     emission_profile::Function, 
-    bin_size::Union{Number,Nothing}=nothing, 
     num_bins::Union{Int,Nothing}=nothing,
+    bin_size::Union{Number,Nothing}=nothing, 
     start::Union{Number,Nothing}=nothing, 
     stop::Union{Number,Nothing}=nothing)
 
@@ -86,89 +86,89 @@ function line_emission_spectrum(
     return binned_fluxes, bins
 end
 
-"""
-    line_emission_spectrum(initial_data, output_data, configurations::VacuumOTEConfigurations; 
-                           emission_profile::Function, bin_size::Number=NaN, num_bins::Int=NaN,
-                           start::Number=NaN, stop::Number=NaN)
+# # """
+# #     line_emission_spectrum(initial_data, output_data, configurations::VacuumOTEConfigurations; 
+# #                            emission_profile::Function, bin_size::Number=NaN, num_bins::Int=NaN,
+# #                            start::Number=NaN, stop::Number=NaN)
 
-Compute the binned intensity of a line emission spectrum.
+# # Compute the binned intensity of a line emission spectrum.
 
-# Arguments
-- `initial_data`: Initial condition data.
-- `output_data`: Output data from the radiation model.
-- `configurations::VacuumOTEConfigurations`: Configuration parameters for the model.
-- `emission_profile::Function`: User-defined function describing the emission profile.
+# # # Arguments
+# # - `initial_data`: Initial condition data.
+# # - `output_data`: Output data from the radiation model.
+# # - `configurations::VacuumOTEConfigurations`: Configuration parameters for the model.
 
-# Keywords
-- `start::Union{Number, Nothing}`: Lower bound of the range to be binned. If unspecified, the minimum of the energy quotients will be used.
-- `stop::Union{Nothing, Number}=nothing`: Upper bound of the range to be binned. If unspecified, the maximum of the energy quotients will be used.
-- `bin_size_conditioner::Number`: Conditioner of the bin size. The bin size is required to be larger than the conditioner times the max variation of the energy factor.
-- `edge_width::Int=3`: Width of the edge to be ignored for bin size conditioning (since edges have unusually large local variations, especially in the presence of light-rings and similar phenomena).
-# Returns
-- `binned_fluxes`: Array of the binned intensity in each bin.
-- `bins`: Array of the bin edges.
-"""
-function line_emission_spectrum(
-    initial_data, 
-    output_data, 
-    configurations::VacuumOTEConfigurations; 
-    emission_profile::Function, 
-    start::Union{Number,Nothing}=nothing, 
-    stop::Union{Number,Nothing}=nothing,
-    bin_size_conditioner::Number,
-    edge_width::Int)
+# # # Keywords
+# # - `emission_profile::Function`: User-defined function describing the emission profile.
+# # - `start::Union{Number, Nothing}`: Lower bound of the range to be binned. If unspecified, the minimum of the energy quotients will be used.
+# # - `stop::Union{Nothing, Number}=nothing`: Upper bound of the range to be binned. If unspecified, the maximum of the energy quotients will be used.
+# # - `bin_size_conditioner::Number`: Conditioner of the bin size. The bin size is required to be larger than the conditioner times the max variation of the energy factor.
+# # - `edge_width::Int=3`: Width of the edge to be ignored for bin size conditioning (since edges have unusually large local variations, especially in the presence of light-rings and similar phenomena).
+# # # Returns
+# # - `binned_fluxes`: Array of the binned intensity in each bin.
+# # - `bins`: Array of the bin edges.
+# # """
+# @kwmethod function line_emission_spectrum(
+#     initial_data, 
+#     output_data, 
+#     configurations::VacuumOTEConfigurations; 
+#     emission_profile::Function, 
+#     start::Union{Number,Nothing}=nothing, 
+#     stop::Union{Number,Nothing}=nothing,
+#     bin_size_conditioner::Number,
+#     edge_width::Int)
 
-    spacetime = configurations.spacetime
-    model = configurations.radiative_model
-    coords_top = coordinates_topology(spacetime)
+#     spacetime = configurations.spacetime
+#     model = configurations.radiative_model
+#     coords_top = coordinates_topology(spacetime)
 
-    cache = get_postprocess_cache(configurations)
-    dump_observer_four_velocity_in!(cache)
+#     cache = get_postprocess_cache(configurations)
+#     dump_observer_four_velocity_in!(cache)
 
-    Nrays = number_of_initial_conditions(configurations)
-    F = zeros(Nrays)
-    q = zeros(Nrays)
-    at_source = zeros(Bool, Nrays)
+#     Nrays = number_of_initial_conditions(configurations)
+#     F = zeros(Nrays)
+#     q = zeros(Nrays)
+#     at_source = zeros(Bool, Nrays)
 
-    for i in 1:Nrays
+#     for i in 1:Nrays
 
-        @views begin 
+#         @views begin 
 
-            pi = initial_data[1:4,i]
-            ki = initial_data[5:8,i]
+#             pi = initial_data[1:4,i]
+#             ki = initial_data[5:8,i]
             
-            pf = output_data[1:4,i]
-            kf = output_data[5:8,i]
+#             pf = output_data[1:4,i]
+#             kf = output_data[5:8,i]
 
-        end
+#         end
 
-        if !is_final_position_at_source(pf, spacetime, model)
-            continue
-        end
-        at_source[i] = true
+#         if !is_final_position_at_source(pf, spacetime, model)
+#             continue
+#         end
+#         at_source[i] = true
 
-        dump_metrics_and_emitter_four_velocity_in!(cache, pi, pf, spacetime, model, coords_top)
-        q[i] = energies_quotient(ki, kf, cache)
-        F[i] = q[i]^3*emission_profile(pf, spacetime, model)
-    end
+#         dump_metrics_and_emitter_four_velocity_in!(cache, pi, pf, spacetime, model, coords_top)
+#         q[i] = energies_quotient(ki, kf, cache)
+#         F[i] = q[i]^3*emission_profile(pf, spacetime, model)
+#     end
 
-    #If start or stop not provided, set to extremes
-    if start===nothing
-        start = minimum(q[at_source])
-    end
-    if stop===nothing
-        stop = maximum(q[at_source])
-    end
+#     #If start or stop not provided, set to extremes
+#     if start===nothing
+#         start = minimum(q[at_source])
+#     end
+#     if stop===nothing
+#         stop = maximum(q[at_source])
+#     end
 
-    num_bins = infer_num_bins(q, at_source, start, stop, bin_size_conditioner, edge_width, configurations.image_plane)
+#     num_bins = infer_num_bins(q, at_source, start, stop, bin_size_conditioner, edge_width, configurations.image_plane)
 
-    bins = create_bins(bin_size=bin_size, num_bins=num_bins, start=start, stop=stop)
-    binned_fluxes = bin_values_and_sum_weights(bins, q[at_source], F[at_source])
+#     bins = create_bins(bin_size=bin_size, num_bins=num_bins, start=start, stop=stop)
+#     binned_fluxes = bin_values_and_sum_weights(bins, q[at_source], F[at_source])
     
-    normalize_by_pixel_area!(binned_fluxes, configurations)
-    normalize_by_image_plane_distance!(binned_fluxes, configurations)
-    return binned_fluxes, bins
-end
+#     normalize_by_pixel_area!(binned_fluxes, configurations)
+#     normalize_by_image_plane_distance!(binned_fluxes, configurations)
+#     return binned_fluxes, bins
+# end
 
 """
     infer_num_bins(q, at_source, start, stop, bin_size_conditioner, edge_width, image_plane)
