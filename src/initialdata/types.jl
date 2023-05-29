dc5d4f47b2ea31c407bc54cb73c9fc949b994b67
Joaquin <pelle.joaquin@gmail.com>
2023-05-29 01:05:59 -0300

    Fix missing save

--- src/initialdata/types.jl
+++ src/initialdata/types.jl
@@ -1,25 +1,3 @@
-abstract type AbstractCamera end
-
-@with_kw struct ImagePlane <: AbstractCamera
-    distance::Float64
-    observer_inclination_in_degrees::Float64
-    horizontal_side::Float64
-    vertical_side::Float64
-    horizontal_number_of_nodes::Int
-    vertical_number_of_nodes::Int
-    observer_inclination_in_radians::Float64 = deg2rad(observer_inclination_in_degrees)
-end
-
-@with_kw struct PinholeCamera <: AbstractCamera 
-    position::Vector{Float64}
-    four_velocity::Vector{Float64}
-    horizontal_aperture::Float64 #This is distance*cos(horizontal_aperture_angle)
-    vertical_aperture::Float64   #This is distance*cos(horizontal_aperture_angle)
-    horizontal_number_of_nodes::Int
-    vertical_number_of_nodes::Int
-    direction::Vector{Float64} = to_center(position, four_velocity)
-end
-
 @with_kw mutable struct OTEInitialDataCache
     metric::Matrix{Float64} = zeros(4,4)
     vector::Vector{Float64} = zeros(4)
