f8c8f391ddfb9efa2eb67db4723e8665bfb876be
Joaquin <pelle.joaquin@gmail.com>
2023-05-29 00:32:47 -0300

    Refactor views functions

--- src/postprocess/views.jl
+++ src/postprocess/views.jl
@@ -1,43 +1,27 @@
 function get_pixel_coordinates_vectors(configurations::AbstractOTEConfigurations)
     image_plane = configurations.image_plane
-
-    sα = image_plane.horizontal_side_image_plane
-    sβ = image_plane.vertical_side_image_plane
-    Nα = image_plane.horizontal_number_of_nodes
-    Nβ = image_plane.vertical_number_of_nodes
-
+    sα, sβ = sides(image_plane)
+    Nα, Nβ = numbers_of_nodes_per_side(image_plane)
     return range(-0.5*sα, stop=0.5*sα; length=Nα), range(-0.5*sβ, stop=0.5*sβ; length=Nβ)
 end
 
-@kwdispatch view_intensities_grid
+@kwdispatch view_as_grid
 
-@kwmethod function view_intensities_grid(output_data, configurations::NonVacuumOTEConfigurations; E_idx)
-    image_plane = configurations.image_plane
-
+@kwmethod function view_as_grid(output_data, configurations::NonVacuumOTEConfigurations; E_idx)
     NE = length(configurations.observed_energies)
-    Nα = image_plane.horizontal_number_of_nodes
-    Nβ = image_plane.vertical_number_of_nodes
-    
-    @views intensities_grid = reshape(output_data[8+NE+E_idx,:], (Nα, Nβ))
-    return intensities_grid
+    Nα, Nβ = numbers_of_nodes_per_side(configurations.image_plane)
+    @views grid = reshape(output_data[8+NE+E_idx,:], (Nα, Nβ))
+    return grid
 end
 
-function view_intensities_grid(intensities, configurations::VacuumOTEConfigurations)
-    image_plane = configurations.image_plane
-
-    Nα = image_plane.horizontal_number_of_nodes
-    Nβ = image_plane.vertical_number_of_nodes
-    
-    @views intensities_grid = reshape(intensities, (Nα, Nβ))
-    return intensities_grid
+function view_as_grid(vector, configurations::VacuumOTEConfigurations)
+    Nα, Nβ = numbers_of_nodes_per_side(configurations.image_plane)
+    @views grid = reshape(vector, (Nα, Nβ))
+    return grid
 end
 
-@kwmethod function view_intensities_grid(intensities, configurations::VacuumOTEConfigurations; E_idx)
-    image_plane = configurations.image_plane
-
-    Nα = image_plane.horizontal_number_of_nodes
-    Nβ = image_plane.vertical_number_of_nodes
-    
+@kwmethod function view_as_grid(intensities, configurations::VacuumOTEConfigurations; E_idx)
+    Nα, Nβ = numbers_of_nodes_per_side(configurations.image_plane)
     @views intensities_grid = reshape(intensities[E_idx,:], (Nα, Nβ))
     return intensities_grid
 end
