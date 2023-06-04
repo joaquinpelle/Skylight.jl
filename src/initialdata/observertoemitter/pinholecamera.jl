eb76381df091f200ec1b9c6bf834035570c33b3c
Joaquin <pelle.joaquin@gmail.com>
2023-06-04 18:53:41 -0300

    Add time offsets and fix triad components calculation

--- src/initialdata/observertoemitter/pinholecamera.jl
+++ src/initialdata/observertoemitter/pinholecamera.jl
@@ -1,24 +1,27 @@
 """
     Builds a bundle of rays set on a tetrad at the camera position. The tetrad has the normalized ∂t as time vector and the central direction
-    projected orthogonally to the time vector as the first spatial vector. The rays have unit energy in this tetrad"""
+    projected orthogonally to the time vector as the first spatial vector. The rays have unit energy in this tetrad. 
+    See docs/pinholecamera.md for more details"""
 function get_initial_data(camera::PinholeCamera, configurations::AbstractOTEConfigurations)
-    
     cache = get_initial_data_cache(camera)
-    rays = my_zeros(configurations)
-
-    @views begin
-        xμ = rays[1:4,:]
-        kμ = rays[5:8,:]
-    end
-
-    position = camera.position
-    set_metric_and_tetrad!(cache, position, configurations)
-    
     @views tetrad = cache.tetrad
+    
+    rays = my_zeros(configurations)    
+    position = copy(camera.position)
 
-    set_rays_position!(xμ, position)
-    set_rays_momenta!(kμ, tetrad, camera)
-    #TODO in the case of the PinholeCamera this should add to the position[1] of camera
+    Npixels = number_of_pixels(camera) 
+    i = 1
+    for t in observation_times(configurations)
+        position[1] += t
+        @views begin
+            xμ = rays[1:4,i:(i-1+Npixels)]
+            kμ = rays[5:8,i:(i-1+Npixels)]
+        end
+        set_metric_and_tetrad!(cache, position, configurations)
+        set_rays_position!(xμ, position)
+        set_rays_momenta!(kμ, tetrad, camera)
+        i += Npixels
+    end
     return rays
 end
 
@@ -41,12 +44,15 @@ function set_rays_tetrad_components!(kμ, camera::PinholeCamera)
 end
 
 function set_rays_triad_components!(kμ, camera::PinholeCamera)
-    @views space_kμ = kμ[2:4,:] 
-    α, β = get_pixel_coordinates_vectors(camera)
-    @. begin
-        space_kμ[1,:] = cos(α)*cos(β)
-        space_kμ[2,:] =-sin(α)*cos(β)
-        space_kμ[3,:] = sin(β)
+    @views space_kμ = kμ[2:4,:]
+    i = 1 
+    for (α, β) in get_pixel_coordinates(camera)
+        @. begin
+            space_kμ[1,i] = cos(α)*cos(β)
+            space_kμ[2,i] = sin(α)*cos(β)
+            space_kμ[3,i] = sin(β)
+        end
+        i += 1
     end
     return nothing        
 end
