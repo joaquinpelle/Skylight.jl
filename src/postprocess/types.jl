28d9ee0ebb08cbc9d51f80507320fa7fa57ebe28
Joaquin <pelle.joaquin@gmail.com>
2023-06-04 20:58:29 -0300

    Add pinhole camera post  process cache

--- src/postprocess/types.jl
+++ src/postprocess/types.jl
@@ -1,6 +1,16 @@
-@with_kw mutable struct OTEPostProcessCache
+abstract type AbstractPostProcessCache end
+
+@with_kw mutable struct ImagePlanePostProcessCache <: AbstractPostProcessCache
     observer_metric::Matrix{Float64} = zeros(4,4)
     emitter_metric::Matrix{Float64} = zeros(4,4)
     observer_four_velocity::Vector{Float64} = zeros(4)
     emitter_four_velocity::Vector{Float64} = zeros(4)
+end
+
+@with_kw mutable struct PinholeCameraPostProcessCache <: AbstractPostProcessCache
+    observer_metric::Matrix{Float64} = zeros(4,4)
+    emitter_metric::Matrix{Float64} = zeros(4,4)
+    observer_four_velocity::Vector{Float64} = zeros(4)
+    emitter_four_velocity::Vector{Float64} = zeros(4)
+    observer_normal::Vector{Float64} = zeros(4)
 end
