47f7d72d65c5ece4e5457f3987d2e8404ec61f31
Joaquin <pelle.joaquin@gmail.com>
2023-06-04 03:29:58 -0300

    Add get_initial_data dispatcher method

--- src/initialdata/initialdata.jl
+++ src/initialdata/initialdata.jl
@@ -4,6 +4,8 @@ include("emittertoobserver/emittertoobserver.jl")
 include("observertoemitter/imageplane.jl")
 include("observertoemitter/pinholecamera.jl")
 
+get_initial_data(configurations::VacuumOTEConfigurations) = get_initial_data(configurations.camera, configurations)
+
 function dump_metric_in!(cache::AbstractInitialDataCache, position, spacetime::AbstractSpacetime)
     set_metric!(cache.metric, position, spacetime)
 end
