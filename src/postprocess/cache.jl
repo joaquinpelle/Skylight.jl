0a8ebab01d85e8574a961768971f3af8143edc66
Joaquin <pelle.joaquin@gmail.com>
2023-06-04 20:58:11 -0300

    Rename OTEPostprocess cache

--- src/postprocess/cache.jl
+++ src/postprocess/cache.jl
@@ -1,11 +1,11 @@
-function set_metrics_and_four_velocities!(cache::OTEPostProcessCache, initial_position, final_position, spacetime, model, coords_top)
+function set_metrics_and_four_velocities!(cache::ImagePlanePostProcessCache, initial_position, final_position, spacetime, model, coords_top)
     set_metric!(cache.observer_metric, initial_position, spacetime) 
     set_metric!(cache.emitter_metric, final_position, spacetime)
     set_static_four_velocity!(cache.observer_four_velocity, cache.observer_metric)
     set_emitter_four_velocity!(cache.emitter_four_velocity, final_position, cache.emitter_metric, spacetime, model, coords_top)
 end
 
-function unpack_views(cache::OTEPostProcessCache)
+function unpack_views(cache::ImagePlanePostProcessCache)
     @views begin
         observer_metric = cache.observer_metric
         emitter_metric = cache.emitter_metric
