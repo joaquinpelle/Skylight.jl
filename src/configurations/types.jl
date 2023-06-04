8aa1f39aaa3e52648593973964896e40697e142c
Joaquin <pelle.joaquin@gmail.com>
2023-06-04 18:54:10 -0300

    Add range assertions to configurations

--- src/configurations/types.jl
+++ src/configurations/types.jl
@@ -11,6 +11,12 @@ abstract type AbstractCamera end
     vertical_side::Float64
     horizontal_number_of_nodes::Int
     vertical_number_of_nodes::Int
+    @assert 0 < distance
+    @assert 0 < observer_inclination_in_degrees <= 90
+    @assert 0 < horizontal_side
+    @assert 0 < vertical_side 
+    @assert 0 < horizontal_number_of_nodes
+    @assert 0 < vertical_number_of_nodes
     observer_inclination_in_radians::Float64 = deg2rad(observer_inclination_in_degrees)
 end
 
@@ -20,6 +26,10 @@ end
     vertical_aperture_in_degrees::Float64   #This is distance*cos(horizontal_aperture_angle)
     horizontal_number_of_nodes::Int
     vertical_number_of_nodes::Int
+    @assert 0 < horizontal_aperture_in_degrees <= 180
+    @assert 0 < vertical_aperture_in_degrees <= 90
+    @assert 0 < horizontal_number_of_nodes
+    @assert 0 < vertical_number_of_nodes
     horizontal_aperture_in_radians::Float64 = deg2rad(horizontal_aperture_in_degrees)
     vertical_aperture_in_radians::Float64 = deg2rad(vertical_aperture_in_degrees)
 end
@@ -31,6 +41,9 @@ end
     observed_times::Vector{Float64}
     observed_energies::Vector{Float64}
     unit_mass_in_solar_masses::Float64
+    @assert all(0 .< observed_times)
+    @assert all(0 .< observed_energies)
+    @assert 0 < unit_mass_in_solar_masses
 end
 
 @with_kw struct VacuumOTEConfigurations{S<:AbstractSpacetime, M<:AbstractRadiativeModel, C<:AbstractCamera} <: AbstractOTEConfigurations
@@ -39,6 +52,8 @@ end
     camera::C
     observed_times::Vector{Float64}
     unit_mass_in_solar_masses::Float64
+    @assert all(0 .< observed_times)
+    @assert 0 < unit_mass_in_solar_masses
 end
 
 @with_kw struct VacuumETOConfigurations{S<:AbstractSpacetime, M<:AbstractRadiativeModel} <: AbstractETOConfigurations
@@ -48,6 +63,10 @@ end
     number_of_packets_per_point::Int
     observer_distance::Float64
     unit_mass_in_solar_masses::Float64
+    @assert 0 < number_of_points
+    @assert 0 < number_of_packets_per_point
+    @assert 0 < observer_distance
+    @assert 0 < unit_mass_in_solar_masses
 end
 
 VacuumConfigurations = Union{VacuumETOConfigurations, VacuumOTEConfigurations}
