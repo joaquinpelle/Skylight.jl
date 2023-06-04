ff945fd5cd356aa3834fe8f6b304e274d41d5b3c
Joaquin <pelle.joaquin@gmail.com>
2023-06-04 02:50:07 -0300

    Add pinhole camera cache

--- src/initialdata/types.jl
+++ src/initialdata/types.jl
@@ -1,6 +1,11 @@
-@with_kw mutable struct OTEInitialDataCache
+@with_kw mutable struct ImagePlaneCache 
     metric::Matrix{Float64} = zeros(4,4)
     vector::Vector{Float64} = zeros(4)
+end
+
+@with_kw mutable struct PinholeCameraCache 
+    metric::Matrix{Float64} = zeros(4,4)
+    tetrad::Vector{Float64} = zeros(4,4)
 end
 
 @with_kw mutable struct ETOInitialDataCache
