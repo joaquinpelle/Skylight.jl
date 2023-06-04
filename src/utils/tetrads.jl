dd19c9ef411592ac29caaae0c573592a1aeede04
Joaquin <pelle.joaquin@gmail.com>
2023-06-04 02:50:31 -0300

    Add dyad orthormalization

--- src/utils/tetrads.jl
+++ src/utils/tetrads.jl
@@ -1,5 +1,4 @@
 function set_triad!(triad, time_vector, metric)
-
     @views begin
         triad_time_components = triad[1,:]
         triad_space_components = triad[2:4,:]  
@@ -8,11 +7,10 @@ function set_triad!(triad, time_vector, metric)
     fill!(triad_time_components,0.0)
     rand!(triad_space_components)
     orthonormalize!(triad, time_vector, metric)
-
+    return nothing
 end
 
-function set_surface_adapted_triad!(triad, time_vector, position, metric, metric_inverse, model, coords_top)
-
+function set_surface_adapted_triad!(triad, time_vector, position, metric, metric_inverse, model::AbstractRadiativeModel, coords_top)
     @views begin
         triad_time_components = triad[1,:]
         normal = triad[:,1]
@@ -25,11 +23,36 @@ function set_surface_adapted_triad!(triad, time_vector, position, metric, metric
 
     set_unit_surface_normal!(normal, position, metric, metric_inverse, model, coords_top) 
     orthonormalize!(triad, time_vector, metric)
-
+    return nothing
 end
 
-function orthonormalize!(triad, time_vector, metric)
+"""
+    orthonormalize!(dyad::Matrix, time_vector::Vector, space_vector::Vector, metric)
 
+    Orthonormalize the dyad `dyad` with respect to the metric `metric` and the vectors `time_vector` and `space_vector`.
+    Both `time_vector` and `space_vector` are assumed to be normalized and orthogonal to each other.
+"""
+function orthonormalize!(dyad, time_vector, space_vector, metric)
+    @views begin
+        e1 = dyad[:,1]
+        e2 = dyad[:,2]
+    end
+
+    e1 .+= time_vector*scalar_product(e1,time_vector,metric) - space_vector*scalar_product(e1,space_vector,metric)
+    normalize_spacelike!(e1,metric)
+
+    e2 .+= time_vector*scalar_product(e2,time_vector,metric) - space_vector*scalar_product(e2,space_vector,metric) - e1*scalar_product(e2,e1,metric)
+    normalize_spacelike!(e2,metric)
+    return nothing
+end
+
+"""
+    orthonormalize!(triad::Matrix, time_vector::Vector, metric)
+
+    Orthonormalize the triad `triad` with respect to the metric `metric` and the vector `time_vector`.
+    `time_vector` is assumed to be normalized.
+"""
+function orthonormalize!(triad, time_vector, metric)
     @views begin
         e1 = triad[:,1]
         e2 = triad[:,2]
@@ -44,17 +67,23 @@ function orthonormalize!(triad, time_vector, metric)
 
     e3 .+= time_vector*scalar_product(e3,time_vector,metric) - e1*scalar_product(e3,e1,metric) - e2*scalar_product(e3,e2,metric)
     normalize_spacelike!(e3,metric)
+    return nothing
+end
 
+function set_unit_time_components!(kμ)
+    kμ[1,:] .= 1.0
+    return nothing
+end
+
+function set_negative_unit_time_components!(kμ)
+    kμ[1,:] .= -1.0
+    return nothing
 end
 
 function set_coordinate_components_from_tetrad_components!(vectors, tetrad)
-
     nvectors = size(vectors,2)
-
     for i in 1:nvectors
-        
         vectors[:,i] = tetrad*vectors[:,i]  
-    
     end
-
+    return nothing
 end
