d4e9fe63ef02bbdff463175fb6d2fa0a9d5c591d
Joaquin <pelle.joaquin@gmail.com>
2023-06-06 15:03:49 -0300

    Remove observation_times field declaration in test

--- test/unit/transfer/callbacks.jl
+++ test/unit/transfer/callbacks.jl
@@ -128,7 +128,6 @@ using Skylight, Test
 
         configurations = VacuumOTEConfigurations(spacetime=spacetime,
                                    camera = camera,
-                                   observation_times = [0.0,1.0],
                                    radiative_model = model,
                                    unit_mass_in_solar_masses=1.0)
 
