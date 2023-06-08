3023c4573f0934e59be7cff377937f29390abdcb
Joaquin <pelle.joaquin@gmail.com>
2023-06-08 19:49:11 -0300

    Declare physical constants and dimensions as const

--- src/utils/constants.jl
+++ src/utils/constants.jl
@@ -4,19 +4,19 @@ module PhysicalConstants
 # Values taken from Particle Data Group "Physical constants" and "Astrophysical constants"
 # https://pdg.lbl.gov/  (last updated  2021)
 
-c = 2.99792458e10         #Speed of light in CGS                      (exact)
-c2 = 8.98755178737e20     #Squared speed of light in CGS              (exact)    
-h = 6.62607015e-27        #Planck constant in CGS  6.626 070 15       (exact)
-h_eV = 4.13566769692e-15  #Planck's constant in eV*s                  (exact*)
-h_bar = 1.054571817e-27   #Reduced Planck constant in CGS             (exact*) 
-k_B = 1.380649e-16        #Boltzmann constant in CGS                  (exact)    
-σ = 5.670374419e-5        #Stefan-Boltzmann constant in CGS           (exact*)
-eV = 1.602176634e-12      #1eV in erg                                 (exact)
-me = 9.109383701528e-28   #Electron mass in CGS 
-ce = 4.80320471257e-10    #Electron charge in esu-CGS and Gaussian-CGS (statC)   (exact*) 
-#ce = 1.602176634e-20     #Electron charge in emu-CGS (a relic, just in case)    (exact)
-G = 6.6743015e-8          #Newton gravitational constant in CGS
-M_sun = 1.988414e33       #Solar mass in CGS
-pc = 3.08567758149e18     #1pc in cm                                             (exact)
+const c = 2.99792458e10         #Speed of light in CGS                      (exact)
+const c2 = 8.98755178737e20     #Squared speed of light in CGS              (exact)    
+const h = 6.62607015e-27        #Planck constant in CGS  6.626 070 15       (exact)
+const h_eV = 4.13566769692e-15  #Planck's constant in eV*s                  (exact*)
+const h_bar = 1.054571817e-27   #Reduced Planck constant in CGS             (exact*) 
+const k_B = 1.380649e-16        #Boltzmann constant in CGS                  (exact)    
+const σ = 5.670374419e-5        #Stefan-Boltzmann constant in CGS           (exact*)
+const eV = 1.602176634e-12      #1eV in erg                                 (exact)
+const me = 9.109383701528e-28   #Electron mass in CGS 
+const ce = 4.80320471257e-10    #Electron charge in esu-CGS and Gaussian-CGS (statC)   (exact*) 
+#const ce = 1.602176634e-20     #Electron charge in emu-CGS (a relic, just in case)    (exact)
+const G = 6.6743015e-8          #Newton gravitational constant in CGS
+const M_sun = 1.988414e33       #Solar mass in CGS
+const pc = 3.08567758149e18     #1pc in cm                                             (exact)
 
 end
--- src/utils/dimensions.jl
+++ src/utils/dimensions.jl
@@ -24,56 +24,56 @@ Base.:^(dim::Dimension, n::Number) = Dimension(L=dim.L*n,
                                                 M=dim.M*n,
                                                 T=dim.T*n)
 
-adim = Dimension()                                                
-length = Dimension(L=1)
-mass = Dimension(M=1)
-time = Dimension(T=1)
+const adim = Dimension()                                                
+const length = Dimension(L=1)
+const mass = Dimension(M=1)
+const time = Dimension(T=1)
 
-action = length^2*mass/time
-angle = adim
-angular_acceleration = time^-2
-angular_momentum = length^2*mass/time
-angular_velocity = angle/time
-area = length^2
-area_density = length^-2*mass
-energy = length^2*mass/time^2
-energy_density = length^-1*mass/time^2
-force = length*mass/time^2
-frequency = time^-1
-heat = length^2*mass/time^2
-impulse = length*mass/time
-linear_density = length^-1*mass
-mass_density = length^-3*mass
-moment_of_inertia = length^2*mass
-momentum = length*mass/time
-power = length^2*mass/time^3
-pressure = length^-1*mass/time^2
-specific_intensity = mass/time^2
-torque = length^2*mass/time^2
-velocity = length/time
-volume = length^3
-wavenumber = length^-1
+const action = length^2*mass/time
+const angle = adim
+const angular_acceleration = time^-2
+const angular_momentum = length^2*mass/time
+const angular_velocity = angle/time
+const area = length^2
+const area_density = length^-2*mass
+const energy = length^2*mass/time^2
+const energy_density = length^-1*mass/time^2
+const force = length*mass/time^2
+const frequency = time^-1
+const heat = length^2*mass/time^2
+const impulse = length*mass/time
+const linear_density = length^-1*mass
+const mass_density = length^-3*mass
+const moment_of_inertia = length^2*mass
+const momentum = length*mass/time
+const power = length^2*mass/time^3
+const pressure = length^-1*mass/time^2
+const specific_intensity = mass/time^2
+const torque = length^2*mass/time^2
+const velocity = length/time
+const volume = length^3
+const wavenumber = length^-1
 
 #Electromagnetical (Gaussian or Lorentz-Heaviside)
-capacitance = length
-charge = length^1.5*mass^0.5/time
-charge_density = length^-1.5*mass^0.5/time
-conductivity = time^-1
-current_density = length^-0.5*mass^0.5*time^-2
-current_intensity = length^1.5*mass^0.5*time^-2
-electric_potential = length^0.5*mass^0.5/time
-electric_field = length^-0.5*mass^0.5/time
-electric_displacement_field = length^-0.5*mass^0.5/time
-inductance = length^-1*time^2
-magnetic_field = length^-0.5*mass^0.5/time
-magnetic_H_field = length^-0.5*mass^0.5/time
-magnetic_diffusivity = length^2/time
-magnetic_dipole_moment = length^2.5*mass^0.5/time
-magnetic_flux = length^1.5*mass^0.5/time
-magnetization = length^0.5*mass^0.5/time^2
-permeability = length^-2*time^2
-permittivity = adim
-resistance = length^-1*time
-resistivity = time
+const capacitance = length
+const charge = length^1.5*mass^0.5/time
+const charge_density = length^-1.5*mass^0.5/time
+const conductivity = time^-1
+const current_density = length^-0.5*mass^0.5*time^-2
+const current_intensity = length^1.5*mass^0.5*time^-2
+const electric_potential = length^0.5*mass^0.5/time
+const electric_field = length^-0.5*mass^0.5/time
+const electric_displacement_field = length^-0.5*mass^0.5/time
+const inductance = length^-1*time^2
+const magnetic_field = length^-0.5*mass^0.5/time
+const magnetic_H_field = length^-0.5*mass^0.5/time
+const magnetic_diffusivity = length^2/time
+const magnetic_dipole_moment = length^2.5*mass^0.5/time
+const magnetic_flux = length^1.5*mass^0.5/time
+const magnetization = length^0.5*mass^0.5/time^2
+const permeability = length^-2*time^2
+const permittivity = adim
+const resistance = length^-1*time
+const resistivity = time
 
 end
