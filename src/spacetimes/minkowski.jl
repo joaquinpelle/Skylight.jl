efd0b41781c3a52f6ca6071e6ba2fbd0d3c08275
Joaquin <pelle.joaquin@gmail.com>
2023-06-09 01:54:29 -0300

    Clean Minkowski file

--- src/spacetimes/minkowski.jl
+++ src/spacetimes/minkowski.jl
@@ -2,21 +2,13 @@ struct MinkowskiSpacetimeCartesianCoordinates <: AbstractSpacetime end
 
 coordinates_topology(::MinkowskiSpacetimeCartesianCoordinates) = CartesianTopology()
 
-function metric!(g, q, spacetime::MinkowskiSpacetimeCartesianCoordinates)
-    
-    """ 
-    g: container for the metric 
-    q: spacetime position
-    """
-
+function metric!(g, position, ::MinkowskiSpacetimeCartesianCoordinates)
     fill!(g,0.0)
     g[1,1] = -1.0
     g[2,2] =  1.0
     g[3,3] =  1.0
     g[4,4] =  1.0
-
     return nothing
-    
 end
 
 metric_inverse!(g, q, spacetime::MinkowskiSpacetimeCartesianCoordinates) = metric!(g, q, spacetime)
@@ -27,40 +19,22 @@ struct MinkowskiSpacetimeSphericalCoordinates <: AbstractSpacetime end
 
 coordinates_topology(::MinkowskiSpacetimeSphericalCoordinates) = SphericalTopology()
 
-function metric!(g, q, spacetime::MinkowskiSpacetimeSphericalCoordinates)
-    
-    """ 
-    g: container for the metric 
-    q: spacetime position
-    """
-
-    t, r, θ, φ = q
-
+function metric!(g, position, ::MinkowskiSpacetimeSphericalCoordinates)
+    t, r, θ, φ = position
     fill!(g,0.0)
     g[1,1] = -1.0
     g[2,2] =  1.0
     g[3,3] =  r^2
     g[4,4] =  r^2*sin(θ)^2
-    
     return nothing
-
 end
 
-function metric_inverse!(g, q, spacetime::MinkowskiSpacetimeSphericalCoordinates)
-    
-    """ 
-    g: container for the metric 
-    q: spacetime position
-    """
-
-    t, r, θ, φ = q
-
+function metric_inverse!(g, position, spacetime::MinkowskiSpacetimeSphericalCoordinates)
+    t, r, θ, φ = position
     fill!(g,0.0)
     g[1,1] = -1.0
     g[2,2] =  1.0
     g[3,3] =  1/r^2
     g[4,4] =  1/(r^2*sin(θ)^2)
-    
     return nothing
-
 end
