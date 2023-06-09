cdb809c6c9d56c98f390da7f5181a4b12ee4d75d
Joaquin <pelle.joaquin@gmail.com>
2023-06-09 17:15:33 -0300

    Delete blank lines

--- src/radiativemodels/syntheticpolarcap.jl
+++ src/radiativemodels/syntheticpolarcap.jl
@@ -1,5 +1,4 @@
 @with_kw struct SyntheticPolarCap <: AbstractSurfaceEmissionModel
-
     star_radius::Float64
     angular_speed::Float64
     misalignment_angle_in_degrees::Float64 
@@ -19,7 +18,6 @@ end
 opaque_interior_surface_trait(::SyntheticPolarCap) = IsOpaqueInteriorSurface()
 
 function surface_differential!(covector, position, ::SyntheticPolarCap, ::CartesianTopology)
-
     @views begin
         t,x,y,z = position
     end
@@ -28,27 +26,19 @@ function surface_differential!(covector, position, ::SyntheticPolarCap, ::Cartes
     covector[2] = 2x
     covector[3] = 2y
     covector[4] = 2z
-
     return nothing
 end
 
 function emitter_four_velocity!(vector, position, metric, spacetime, model::SyntheticPolarCap, coords_top)
-        
     angular_speed = model.angular_speed
     tangent_vector_zaxis_rotation!(vector, position, angular_speed, metric, coords_top)
-    
     return nothing
 end
 
 function space_positions(npoints, model::SyntheticPolarCap, coords_top::CartesianTopology)
-
     space_pos = zeros(3, npoints)
-
     random_uniform_points_unit_spherical_cap!(space_pos, model.angular_radius_in_degrees, coords_top)
-
     rotate_around_y_axis!(space_pos, model.misalignment_angle_in_degrees)
-
     space_pos .*= model.star_radius
-
     return space_pos
 end
