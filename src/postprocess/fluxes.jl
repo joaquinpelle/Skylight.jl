30fcf3f082579abe862820f4230dc9f531892314
Joaquin <pelle.joaquin@gmail.com>
2023-06-10 19:05:55 -0300

    Fix normalization calls in fluxes

--- src/postprocess/fluxes.jl
+++ src/postprocess/fluxes.jl
@@ -1,16 +1,17 @@
-function fluxes(Iobs, camera::ImagePlane)
+function fluxes(Iobs, configurations, ::ImagePlane)
     Fobs = copy(Iobs)
-    normalize_by_pixel_area!(Fobs, camera)
-    normalize_by_image_plane_distance!(Fobs, camera)
+    normalize_by_pixel_area!(Fobs, configurations)
+    normalize_by_image_plane_distance!(Fobs, configurations)
 end
 
 """For monochromatic / bolometric fluxes"""
-function fluxes(I::AbstractVector, camera::PinholeCamera, initial_data::AbstractMatrix, output_data::AbstractMatrix, spacetime::AbstractSpacetime; observer_four_velocity=nothing, flux_direction=nothing)
+function fluxes(I::AbstractVector, configurations, camera::PinholeCamera, initial_data::AbstractMatrix, output_data::AbstractMatrix; observer_four_velocity=nothing, flux_direction=nothing)
 
     same_size(initial_data, output_data) || throw(DimensionMismatch("initial_data and output_data must have the same size."))
     size(initial_data, 2) == length(I) || throw(DimensionMismatch("The number of rays in the initial and output data must be the same as the length of I."))
     number_of_pixels(camera) == length(I) || throw(DimensionMismatch("The number of pixels in the camera must be the same as the length of I."))
 
+    spacetime = configurations.spacetime
     cache = postprocess_cache(camera)
     observer_metric!(cache, camera.position, spacetime)
     observer_four_velocity!(cache, observer_four_velocity) 
--- src/postprocess/spectrum.jl
+++ src/postprocess/spectrum.jl
@@ -1,5 +1,5 @@
 spectrum(initial_data, output_data, configurations::VacuumOTEConfigurations; Emin::Number, Emax::Number, NE::Integer, kwargs...) = spectrum(initial_data, output_data, configurations, range(Emin, stop=Emax, length=NE); kwargs...)
-spectrum(initial_data, output_data, configurations::VacuumOTEConfigurationsm, energies; kwargs...) = spectrum(initial_data, output_data, configurations, configurations.camera, energies; kwargs...)
+spectrum(initial_data, output_data, configurations::VacuumOTEConfigurations, energies; kwargs...) = spectrum(initial_data, output_data, configurations, configurations.camera, energies; kwargs...)
 
 function spectrum(initial_data, 
                 output_data, 
@@ -9,7 +9,7 @@ function spectrum(initial_data,
                 kwargs...)
     same_size(initial_data, output_data) || throw(DimensionMismatch("initial_data and output_data must have the same size."))
     Iobs, _ = observed_specific_intensities(initial_data, output_data, configurations, energies; kwargs...)
-    Fobs = fluxes(Iobs, camera)
+    Fobs = fluxes(Iobs, configurations, camera)
     return sum(Fobs, dims=2)
 end
 
@@ -22,6 +22,6 @@ function spectrum(initial_data,
     
     same_size(initial_data, output_data) || throw(DimensionMismatch("initial_data and output_data must have the same size."))
     Iobs, _ = observed_specific_intensities(initial_data, output_data, configurations, energies; kwargs...)
-    Fobs = fluxes(Iobs, camera, initial_data, output_data, configurations.spacetime; kwargs...)
+    Fobs = fluxes(Iobs, configurations, camera, initial_data, output_data; kwargs...)
     return sum(Fobs, dims=2)
 end
