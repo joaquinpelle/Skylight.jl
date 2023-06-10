4aef2c582bdc359e4bb053e35f3aca25e922c7e7
Joaquin <pelle.joaquin@gmail.com>
2023-06-10 00:00:57 -0300

    Fix black hole callbacks dispatch

--- src/transfer/callbacks/black_hole_accretion_disk.jl
+++ src/transfer/callbacks/black_hole_accretion_disk.jl
@@ -21,32 +21,47 @@ end
     @assert rmax > outer_radius "rmax must be larger than outer_radius"
 end
 
-callback(::AbstractBlackHoleSpacetime, ::AbstractAccretionDisk, ::CartesianTopology) = black_hole_accretion_disk_cartesian_coordinates_callback()
 callback(::AbstractBlackHoleSpacetime, ::AbstractAccretionDisk, ::SphericalTopology) = black_hole_accretion_disk_spherical_coordinates_callback()
+callback(::SchwarzschildSpacetimeKerrSchildCoordinates, ::AbstractAccretionDisk, ::CartesianTopology) = nonspinning_black_hole_accretion_disk_cartesian_coordinates_callback()
+callback(::KerrSpacetimeKerrSchildCoordinates, ::AbstractAccretionDisk, ::CartesianTopology) = spinning_black_hole_accretion_disk_cartesian_coordinates_callback()
 
-black_hole_accretion_disk_cartesian_coordinates_callback() = VectorContinuousCallback(black_hole_accretion_disk_cartesian_coordinates_condition, black_hole_accretion_disk_cartesian_coordinates_affect!, 2)
+spinning_black_hole_accretion_disk_cartesian_coordinates_callback() = VectorContinuousCallback(spinning_black_hole_accretion_disk_cartesian_coordinates_condition, spinning_black_hole_accretion_disk_cartesian_coordinates_affect!, 2)
+nonspinning_black_hole_accretion_disk_cartesian_coordinates_callback() = VectorContinuousCallback(nonspinning_black_hole_accretion_disk_cartesian_coordinates_condition, nonspinning_black_hole_accretion_disk_cartesian_coordinates_affect!, 2)
 black_hole_accretion_disk_spherical_coordinates_callback() = VectorContinuousCallback(black_hole_accretion_disk_spherical_coordinates_condition, black_hole_accretion_disk_spherical_coordinates_affect!, 2)
 
-function black_hole_accretion_disk_cartesian_coordinates_condition(out, u, t, integrator)
-    
-    a = integrator.p.spacetime.a
-
+function nonspinning_black_hole_accretion_disk_cartesian_coordinates_condition(out, u, t, integrator)
     rmax = integrator.p.cbp.rmax
     rmin = integrator.p.cbp.rmin
-    
-    rho2_a2 = u[2]*u[2] + u[3]*u[3] + u[4]*u[4]-a*a
-    r2 = 0.5*(rho2_a2+sqrt(rho2_a2*rho2_a2+4.0*a*a*u[4]*u[4]))
-
+    r2 = u[2]*u[2] + u[3]*u[3] + u[4]*u[4]
     out[1] = u[4]
     out[2] = (rmax*rmax-r2)*(r2-rmin*rmin)
 end
 
-function black_hole_accretion_disk_cartesian_coordinates_affect!(integrator, idx)
-    
+function nonspinning_black_hole_accretion_disk_cartesian_coordinates_affect!(integrator, idx)
+    inner_radius = integrator.p.cbp.inner_radius
+    outer_radius = integrator.p.cbp.outer_radius
+    if idx==1
+        r2 = integrator.u[2]*integrator.u[2] + integrator.u[3]*integrator.u[3] + integrator.u[4]*integrator.u[4]
+        if inner_radius*inner_radius <= r2 <= outer_radius*outer_radius terminate!(integrator) end
+    elseif idx==2 
+        terminate!(integrator)
+    end
+end
+
+function spinning_black_hole_accretion_disk_cartesian_coordinates_condition(out, u, t, integrator)
+    a = integrator.p.spacetime.a
+    rmax = integrator.p.cbp.rmax
+    rmin = integrator.p.cbp.rmin
+    rho2_a2 = u[2]*u[2] + u[3]*u[3] + u[4]*u[4]-a*a
+    r2 = 0.5*(rho2_a2+sqrt(rho2_a2*rho2_a2+4.0*a*a*u[4]*u[4]))
+    out[1] = u[4]
+    out[2] = (rmax*rmax-r2)*(r2-rmin*rmin)
+end
+
+function spinning_black_hole_accretion_disk_cartesian_coordinates_affect!(integrator, idx)
     a = integrator.p.spacetime.a
     inner_radius = integrator.p.cbp.inner_radius
     outer_radius = integrator.p.cbp.outer_radius
-
     if idx==1
         rho2_a2 = integrator.u[2]*integrator.u[2] + integrator.u[3]*integrator.u[3] + integrator.u[4]*integrator.u[4]-a*a
         r2 = 0.5*(rho2_a2+sqrt(rho2_a2*rho2_a2+4.0*a*a*integrator.u[4]*integrator.u[4]))
@@ -57,20 +72,15 @@ function black_hole_accretion_disk_cartesian_coordinates_affect!(integrator, idx
 end
 
 function black_hole_accretion_disk_spherical_coordinates_condition(out, u, t, integrator)
-    
     rmax = integrator.p.cbp.rmax
     rmin = integrator.p.cbp.rmin
-
     out[1] = u[3]-π/2
     out[2] = (rmax-u[2])*(u[2]-rmin)
-
 end
 
 function black_hole_accretion_disk_spherical_coordinates_affect!(integrator, idx)
-
     inner_radius = integrator.p.cbp.inner_radius
     outer_radius = integrator.p.cbp.outer_radius
-
     if (inner_radius <= integrator.u[2] <= outer_radius) || idx == 2
         terminate!(integrator) 
     end
