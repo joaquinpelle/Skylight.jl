e17985cd752270f162b43a25d90ca84af47ee43f
Joaquin <pelle.joaquin@gmail.com>
2023-06-10 23:09:30 -0300

    Add pc to cm and eV to erg conversions

--- src/utils/units.jl
+++ src/utils/units.jl
@@ -21,3 +21,14 @@ function geometrized_unit_magnitude_in_CGS(dim; M1)
 
     return L1_cgs^dim.L*M1_cgs^dim.M*T1_cgs^dim.T
 end
+
+pc_to_cm(d) = d*PhysicalConstants.pc
+cm_to_pc(d) = d/PhysicalConstants.pc
+eV_to_erg(E) = E*PhysicalConstants.eV
+erg_to_eV(E) = E/PhysicalConstants.eV
+keV_to_erg(E) = E*(1e3*PhysicalConstants.eV)
+erg_to_keV(E) = E/(1e3*PhysicalConstants.eV)
+per_eV_to_per_erg(E) = erg_to_eV(E)
+per_erg_to_per_eV(E) = eV_to_erg(E)
+per_keV_to_per_erg(E) = erg_to_keV(E)
+per_erg_to_per_keV(E) = keV_to_erg(E)
