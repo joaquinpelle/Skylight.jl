47de85f4a7de3632e7e2c34918d420657f6bf005
Joaquin <pelle.joaquin@gmail.com>
2023-07-03 15:27:37 -0300

    Fix pinhole camera multithreading

--- src/initialdata/observertoemitter/pinholecamera.jl
+++ src/initialdata/observertoemitter/pinholecamera.jl
@@ -41,7 +41,9 @@ end
 function rays_triad_components!(kμ, camera::PinholeCamera)
     @views space_kμ = kμ[2:4,:] 
     @inbounds begin
-        @threads for (i, (α, β)) in enumerate(grid(camera))
+        pixels = (collect∘grid)(camera)
+        @threads for i in eachindex(pixels)
+            α, β = pixels[i]
             space_kμ[1,i] = cos(α)*cos(β)
             space_kμ[2,i] = sin(α)*cos(β)
             space_kμ[3,i] = sin(β)
