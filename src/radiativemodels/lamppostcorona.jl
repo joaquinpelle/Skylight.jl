386dfd0a4d5fb301f2a60f9816b6be72ba1b76e9
joaquinpelle <pelle.joaquin@gmail.com>
2023-07-28 12:31:01 -0300

    Add lamppost corona model

---
+++ src/radiativemodels/lamppostcorona.jl
@@ -0,0 +1,16 @@
+@with_kw struct LamppostCorona <: AbstractRadiativeModel
+    height::Float64
+    @assert height > 0.0 "height must be positive"
+end
+
+function space_positions(npoints, model::LamppostCorona, ::SphericalTopology)
+    space_pos = zeros(3, npoints)
+    space_pos[1,:] .= model.height
+    space_pos[2,:] .= eps()
+    return space_pos
+end
+
+function emitter_four_velocity!(vector, position, metric, spacetime, ::LamppostCorona, coords_top)
+    static_four_velocity!(vector, metric)
+    return nothing
+end
