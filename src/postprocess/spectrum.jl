a0cbf31a049daac3aae0e56e016eaabaa713d6f5
joaquinpelle <pelle.joaquin@gmail.com>
2023-08-09 16:21:34 -0300

    Remove unnecessary double loop in pinhole spectrum

--- src/postprocess/spectrum.jl
+++ src/postprocess/spectrum.jl
@@ -17,10 +17,54 @@ function spectrum(initial_data,
                 configurations::VacuumOTEConfigurations,
                 camera::PinholeCamera,
                 energies; 
-                kwargs...)
+                observer_four_velocity=nothing, 
+                flux_direction=nothing, 
+                chunks_per_thread::Int=2)
     
     same_size(initial_data, output_data) || throw(DimensionMismatch("initial_data and output_data must have the same size."))
-    Fobs, _ = observed_specific_intensities(initial_data, output_data, configurations, energies; kwargs...)
-    fluxes!(Fobs, configurations, camera, initial_data, output_data; kwargs...)
+    eight_components(initial_data, output_data) || throw(DimensionMismatch("The initial and output data must have eight components."))
+    number_of_pixels(camera) == size(initial_data,2) || throw(DimensionMismatch("The number of pixels in the camera must be the same as the number of data rows."))
+    
+    spacetime = configurations.spacetime
+    model = configurations.radiative_model
+    coords_top = coordinates_topology(spacetime)
+    nrays = size(initial_data, 2)
+    NE = length(energies)
+    q = zeros(nrays)
+    Fobs = zeros(NE, nrays)
+    dΩ = pixel_solid_angles(camera)
+    # Break the work into chunks. More chunks per thread has better load balancing but more overhead
+    nchunks = div(nrays, nthreads()*chunks_per_thread)
+    chunks = Iterators.partition(1:nrays, nchunks)
+    # Map over the chunks, creating an array of spawned tasks. Sync to wait for the tasks to finish.
+    @sync map(chunks) do chunk
+        Threads.@spawn begin
+            cache = postprocess_cache(configurations)
+            observer_metric!(cache, camera.position, spacetime)
+            observer_four_velocity!(cache, observer_four_velocity) 
+            flux_direction!(cache, flux_direction, camera, spacetime) 
+            for i in chunk
+                @views begin 
+                    ki = initial_data[5:8,i]
+                    pf = output_data[1:4,i]
+                    kf = output_data[5:8,i]
+                end
+                if !is_final_position_at_source(pf, spacetime, model)
+                    continue
+                end
+                emitter_metric_and_four_velocity!(cache, pf, spacetime, model, coords_top)
+                q[i] = energies_quotient(ki, kf, cache)
+                for j in axes(energies, 1)
+                    emitted_energy = energies[j]/q[i]
+                    #The difference with the ETO scheme here should be the minus sign in front of the final momentum
+                    #at get emitted intensity, and the is_final_position_at_source call (at observer in ETO)...
+                    Fem = emitted_specific_intensity(pf, -kf, emitted_energy, cache.emitter_four_velocity, cache.emitter_metric, spacetime, model, coords_top, cache.model_cache)
+                    nu = scalar_product(ki, cache.observer_four_velocity, cache.observer_metric)
+                    nn = scalar_product(ki, cache.flux_direction, cache.observer_metric)
+                    Fobs[j, i] = q[i]^3*Fem*nu*nn*dΩ[i]            
+                end
+            end
+        end
+    end
     return sum(Fobs, dims=2)[:,1]
 end
