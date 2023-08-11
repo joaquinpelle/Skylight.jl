63230673005c193f792fbae0e0a5e0929b03da09
joaquinpelle <pelle.joaquin@gmail.com>
2023-08-11 14:30:51 -0300

    Add synchrotron fit parameters interpolation

--- src/radiativemodels/types.jl
+++ src/radiativemodels/types.jl
@@ -1,9 +1,3 @@
-abstract type AbstractRadiativeProcess end
-struct Bremsstrahlung <: AbstractRadiativeProcess end
-struct Synchrotron <: AbstractRadiativeProcess end
-struct InverseCompton <: AbstractRadiativeProcess end
-struct ThermalEmission <: AbstractRadiativeProcess end
-
 abstract type AbstractRadiativeModel end
 abstract type AbstractSurfaceEmissionModel <: AbstractRadiativeModel end
 
@@ -15,3 +9,14 @@ struct IsNotOpaqueInteriorSurface <: OpaqueInteriorSurfaceTrait end
 
 #Set by default the radiative model to IsNotOpaqueInteriorSurface()
 opaque_interior_surface_trait(::AbstractRadiativeModel) = IsNotOpaqueInteriorSurface()
+
+abstract type AbstractRadiativeProcess end
+struct ThermalEmission <: AbstractRadiativeProcess end
+struct Bremsstrahlung <: AbstractRadiativeProcess end
+
+@with_kw struct Synchrotron{I} <: AbstractRadiativeProcess 
+    T::Vector{Float64} = [5e8, 1e9, 2e9, 4e9, 8e9, 1e10, 3e10]
+    α::I = LinearInterpolation([0.0431, 1.121, 1.180, 1.045, 0.9774, 0.9768, 0.9788], T)
+    β::I = LinearInterpolation([10.44, -10.65, -4.008, -0.1897, 1.160, 1.095, 1.021], T)
+    γ::I = LinearInterpolation([16.61, 9.169, 1.559, 0.0595, 0.2641, 0.8332, 1.031], T)
+end
