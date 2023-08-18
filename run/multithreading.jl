9ba276693cb35969a245fe216622794992e655fe
joaquinpelle <pelle.joaquin@gmail.com>
2023-08-18 11:03:13 -0300

    Add intensities test to multithreading

--- run/multithreading.jl
+++ run/multithreading.jl
@@ -18,6 +18,7 @@ function time_intensities(; N, tasks_per_thread)
     cb, cbp = callback_setup(configurations; rhorizon_bound=2e-1) #... or, define your own cb and cbp
     run = integrate(initial_data, configurations, cb, cbp; method=VCABM(), reltol=1e-5, abstol=1e-5)
     output_data = run.output_data
+    println("N = $N")
     println("Serial")
     @time _ = Skylight.observed_bolometric_intensities_serial(initial_data, output_data, configurations, camera)
     println("Multithread")
@@ -25,6 +26,29 @@ function time_intensities(; N, tasks_per_thread)
     println("Multithread tmap")
     @time _ = Skylight.observed_bolometric_intensities_tmap(initial_data, output_data, configurations, camera; tasks_per_thread=tasks_per_thread)
     return nothing
+end
+
+function test_intensities(; N, tasks_per_thread)
+    spacetime = SchwarzschildSpacetimeSphericalCoordinates(M=1.0)
+    camera = ImagePlane(distance = 200.0,
+                            observer_inclination_in_degrees = 5,
+                            horizontal_side = 20.0,
+                            vertical_side = 20.0,
+                            horizontal_number_of_pixels = N,
+                            vertical_number_of_pixels = N)
+    model = NovikovThorneDisk(inner_radius=6.0, outer_radius=18.0)
+    configurations = VacuumOTEConfigurations(spacetime=spacetime,
+                                    camera = camera,
+                                    radiative_model = model,
+                                    unit_mass_in_solar_masses=1.0)
+    initial_data = initialize(configurations)
+    cb, cbp = callback_setup(configurations; rhorizon_bound=2e-1) #... or, define your own cb and cbp
+    run = integrate(initial_data, configurations, cb, cbp; method=VCABM(), reltol=1e-5, abstol=1e-5)
+    output_data = run.output_data
+    println("N = $N")
+    Iobs = observed_bolometric_intensities(initial_data, output_data, configurations, camera; tasks_per_thread = tasks_per_thread)
+    Itmap = Skylight.observed_bolometric_intensities_tmap(initial_data, output_data, configurations, camera; tasks_per_thread = tasks_per_thread)
+    all(Iobs .== Itmap) || error("Iobs != Itmap") 
 end
 
 function btime_intensities(; N, tasks_per_thread)
@@ -44,6 +68,7 @@ function btime_intensities(; N, tasks_per_thread)
     cb, cbp = callback_setup(configurations; rhorizon_bound=2e-1) #... or, define your own cb and cbp
     run = integrate(initial_data, configurations, cb, cbp; method=VCABM(), reltol=1e-5, abstol=1e-5)
     output_data = run.output_data
+    println("N = $N")
     println("Serial")
     @btime _ = Skylight.observed_bolometric_intensities_serial($initial_data, $output_data, $configurations, $camera)
     println("Multithread")
@@ -62,3 +87,4 @@ btime_intensities(N=50, tasks_per_thread=1)
 btime_intensities(N=100, tasks_per_thread=2) 
 time_intensities(N=600, tasks_per_thread=2) 
 time_intensities(N=1000, tasks_per_thread=2)
+test_intensities(N=200, tasks_per_thread=2)
