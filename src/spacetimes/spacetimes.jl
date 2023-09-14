6c4286866ff9a6cc99f3fc3e309f86cb4547526e
joaquinpelle <pelle.joaquin@gmail.com>
2023-09-14 18:58:36 -0300

    Reorganize spacetimes file

--- src/spacetimes/spacetimes.jl
+++ src/spacetimes/spacetimes.jl
@@ -1,25 +1,204 @@
-#Required
+#Required interface
 function coordinates_topology(spacetime)
     error("Coordinates topology not defined for this spacetime.")
 end
 
 """
-    metric!(g, position, spacetime, cache)
+    metric!(g::AbstractMatrix, position::AbstractVector, spacetime::AbstractSpacetime, cache::Union{Nothing, AbstractSpacetimeCache})
 
-Evaluate the spacetime metric at the given position and store the result in the given array using cache for temporary storage.  
+Evaluate the spacetime metric at the given position and store the result in `g` using `cache` for temporary storage.  
 """
 metric!(g, position, spacetime, cache) = error("Metric not defined for this spacetime.")
-function metric!(g, position, spacetime::AbstractSpacetime, ::Nothing)
-    metric!(g, position, spacetime)
-end
 
-#Optional
 """
-    allocate_cache(spacetime)
+    allocate_cache(spacetime::AbstractSpacetime)
 
 Allocate a cache object for the given spacetime. The cache object is used to store temporary data in spacetime-related calculations.
 """
 allocate_cache(::AbstractSpacetime) = nothing
+
+include("coordinatealias.jl")
+include("autodiff.jl")
+include("minkowski.jl")
+include("schwarzschild.jl")
+include("kerr.jl")
+include("frkerr.jl")
+include("johannsen.jl")
+include("chargedwormhole.jl")
+include("bosonstar.jl")
+include("rar.jl")
+include("numerical.jl")
+include("constantsmotion.jl")
+
+"""
+    metric(position::AbstractVector, spacetime::AbstractSpacetime)
+
+Evaluate the spacetime metric at the given position and return the result.
+"""
+function metric(position::AbstractVector, spacetime::AbstractSpacetime)
+    g = zeros(4, 4)
+    cache = allocate_cache(spacetime)
+    metric!(g, position, spacetime, cache)
+    return g
+end
+
+"""
+    metric(position::AbstractVector, spacetime::AbstractSpacetime, cache::Union{Nothing, AbstractSpacetimeCache})
+
+Evaluate the spacetime metric at the given position and return the result using a cache object as temporary storage.
+The cache must be pre-allocated as `cache = allocate_cache(spacetime)`. 
+
+See also [`allocate_cache`](@ref). 
+"""
+function metric(position::AbstractVector, spacetime::AbstractSpacetime, cache::Union{Nothing, AbstractSpacetimeCache})
+    g = zeros(4, 4)
+    metric!(g, position, spacetime, cache)
+    return g
+end
+
+function metric!(g::AbstractMatrix, position::AbstractVector, spacetime::AbstractSpacetime, ::Nothing)
+    metric!(g, position, spacetime)
+end
+
+function metric_field(spacetime::AbstractSpacetime)
+    (g, position) -> metric!(g, position, spacetime)
+end
+
+function metric_field(spacetime::AbstractSpacetime, cache::AbstractSpacetimeCache)
+    (g, position) -> metric!(g, position, spacetime, cache)
+end
+
+function metric_field(spacetime::AbstractSpacetime, ::Nothing)
+    metric_field(spacetime)
+end
+
+"""
+    metric_inverse(position::AbstractVector, spacetime::AbstractSpacetime)
+
+Evaluate the inverse of the metric at the given position and return the result.
+"""
+function metric_inverse(position::AbstractVector, spacetime::AbstractSpacetime)
+    ginv = zeros(4, 4)
+    g = zeros(4, 4)
+    cache = allocate_cache(spacetime)
+    metric_inverse!(ginv, position, spacetime, g, cache)
+    return ginv
+end
+
+"""
+    metric_inverse!(ginv::AbstractMatrix, position::AbstractVector, spacetime::AbstractSpacetime, g::AbstractMatrix, cache::Union{Nothing, AbstractSpacetimeCache})
+
+Evaluate the inverse of the metric at the given position  and store the result in the given array, using `g` and `cache` for temporary storage, which
+must be preallocated as `g = zeros(4,4)` and `cache = allocate_cache(spacetime)`.
+
+See also [`allocate_cache`](@ref). 
+"""
+function metric_inverse!(ginv::AbstractMatrix, position::AbstractVector, spacetime::AbstractSpacetime, g::AbstractMatrix, cache::Union{Nothing, AbstractSpacetimeCache})
+    metric!(g, position, spacetime, cache)
+    inv4x4sym!(ginv, g)
+    return nothing
+end
+
+function metric_inverse!(ginv::AbstractMatrix, position::AbstractVector, spacetime::AbstractSpacetime, g::AbstractMatrix, ::Nothing)
+    metric_inverse!(ginv, position, spacetime, g)
+end
+
+function metric_inverse!(ginv::AbstractMatrix, position::AbstractVector, spacetime::AbstractSpacetime, g::AbstractMatrix)
+    metric!(g, position, spacetime)
+    inv4x4sym!(ginv, g)
+    return nothing
+end
+
+@doc raw"""
+    volume_element(position::AbstractVector, spacetime::AbstractSpacetime, g::AbstractMatrix, cache::Union{Nothing, AbstractSpacetimeCache})
+
+Compute $\sqrt{-\text{det}(g)}$ at the given position, using `g` and `cache` for temporary storage, which must be preallocated as `g = zeros(4,4)` and `cache = allocate_cache(spacetime)`. 
+
+See also [`allocate_cache`](@ref). 
+"""
+function volume_element(position::AbstractVector, spacetime::AbstractSpacetime, g::AbstractMatrix, cache::Union{Nothing, AbstractSpacetimeCache})
+    metric!(g, position, spacetime, cache)
+    return sqrt(-det4x4sym(g))
+end
+
+@doc raw"""
+    volume_element(position::AbstractVector, spacetime::AbstractSpacetime)
+
+Compute $\sqrt{-\text{det}(g)}$ at the given position 
+"""
+function volume_element(position::AbstractVector, spacetime::AbstractSpacetime)
+    g = zeros(4, 4)
+    cache = allocate_cache(spacetime)
+    metric!(g, position, spacetime, cache)
+    return sqrt(-det4x4sym(g))
+end
+
+@doc raw"""
+    christoffel(position::AbstractVector, spacetime::AbstractSpacetime)
+
+Evaluate the Christoffel symbols of the second kind at the given position and return the result as a `4x4x4` array 
+`Γ`, where `Γ[α,μ,ν]` corresponds to 
+
+``\Gamma^\alpha_{\mu \nu} = \frac{1}{2} g^{\alpha \rho}(\partial_\mu g_{\rho \nu} + \partial_\nu g_{\rho \mu} - \partial_\rho g_{\mu \nu}).``
+"""
+function christoffel(position::AbstractVector, spacetime::AbstractSpacetime)
+    Γ = zeros(4, 4, 4)
+    cache = allocate_christoffel_cache(spacetime)
+    christoffel!(Γ, position, spacetime, cache)
+    return Γ
+end
+
+function christoffel(position::AbstractVector,
+    spacetime::AbstractSpacetime,
+    cache::Union{Nothing,AbstractChristoffelCache})
+    Γ = zeros(4, 4, 4)
+    christoffel!(Γ, position, spacetime, cache)
+    return Γ
+end
+
+@doc raw"""
+    christoffel!(Γ::AbstractArray, position::AbstractVector, spacetime::AbstractSpacetime, cache::Union{Nothing,AbstractChristoffelCache})
+
+Evaluate the Christoffel symbols of the second kind at the given position and store the result in
+`Γ`, where `Γ[α,μ,ν]` corresponds to 
+
+``\Gamma^\alpha_{\mu \nu} = \frac{1}{2} g^{\alpha \rho}(\partial_\mu g_{\rho \nu} + \partial_\nu g_{\rho \mu} - \partial_\rho g_{\mu \nu}).``
+
+The cache must be preallocated as `cache = allocate_christoffel_cache(spacetime)`.
+
+See also [`allocate_christoffel_cache`](@ref).
+"""
+function christoffel!(::AbstractArray, ::AbstractVector, ::AbstractSpacetime, ::Union{Nothing,AbstractChristoffelCache})
+    error("Christoffel symbols not defined for this spacetime. This is weird because the behaviour should default to automatic differentiation.
+    Contact the author for help.")
+end
+
+function christoffel!(Γ::AbstractArray, position::AbstractVector, spacetime::AbstractSpacetime, ::Nothing)
+    christoffel!(Γ, position, spacetime)
+end
+
+"""
+    allocate_christoffel_cache(spacetime::AbstractSpacetime)
+
+Allocate a cache object for Christoffel symbols calculations.
+
+See also [`christoffel!`](@ref). 
+""" 
+function allocate_christoffel_cache(spacetime::AbstractSpacetime)
+    AutoDiffChristoffelCache(spacetime)
+end
+
+function AutoDiffChristoffelCache(spacetime::AbstractSpacetime)
+    spacetime_cache = allocate_cache(spacetime)
+    spacetime_metric_field = metric_field(spacetime, spacetime_cache)
+    cfg = ForwardDiff.JacobianConfig(spacetime_metric_field,
+        Matrix{Float64}(undef, 4, 4),
+        Vector{Float64}(undef, 4),
+        ForwardDiff.Chunk{4}())
+    return AutoDiffChristoffelCache(spacetime_metric_field = spacetime_metric_field,
+        spacetime_cache = spacetime_cache,
+        cfg = cfg)
+end
 
 """
     radius(position, spacetime)
@@ -67,38 +246,7 @@ function circular_geodesic_angular_speed(position, spacetime, rotation_sense)
     error("Circular geodesic angular speed not defined for this spacetime.")
 end
 
-"""
-    christoffel!(Γ, position, spacetime, cache)
-
-Evaluate the Christoffel symbols at the given position and store the result in the given array using cache for temporary storage.
-Unless a more specific method is defined, automatic differentiation is used to compute the Christoffel symbols.
-""" 
-function christoffel!(Γ, position, spacetime::AbstractSpacetime, ::Nothing)
-    christoffel!(Γ, position, spacetime)
-end
-
-"""
-    allocate_christoffel_cache(spacetime)
-
-Allocate a cache object for the Christoffel symbols calculation.
-Unless a more specific method is defined, an instance of [`Skylight.AutoDiffChristoffelCache`](@ref) is returned.
-""" 
-function allocate_christoffel_cache(spacetime::AbstractSpacetime)
-    AutoDiffChristoffelCache(spacetime)
-end
-
-"""
-    metric_inverse!(ginv, position, spacetime, g, cache)
-
-Evaluate the inverse of the metric at the given position  and store the result in the given array, using `g` and `cache` for temporary storage.
-Unless a more specialized method is defined, a fast inversion method for 4x4 symmetric matrices is used.
-"""
-function metric_inverse!(ginv, position, spacetime::AbstractSpacetime, g, cache)
-    metric!(g, position, spacetime, cache)
-    inv4x4sym!(ginv, g)
-    return nothing
-end
-
+#Other functions
 #By default we set non-stationarity and non-spherical symmetry  
 """
     stationarity(spacetime)
@@ -124,117 +272,13 @@ function axial_symmetry(spacetime::AbstractSpacetime)
     IsNotAxiallySymmetric()
 end
 
-include("coordinatealias.jl")
-include("autodiff.jl")
-include("minkowski.jl")
-include("schwarzschild.jl")
-include("kerr.jl")
-include("frkerr.jl")
-include("johannsen.jl")
-include("chargedwormhole.jl")
-include("bosonstar.jl")
-include("rar.jl")
-include("numerical.jl")
-include("constantsmotion.jl")
-
-function AutoDiffChristoffelCache(spacetime::AbstractSpacetime)
-    spacetime_cache = allocate_cache(spacetime)
-    spacetime_metric_field = metric_field(spacetime, spacetime_cache)
-    cfg = ForwardDiff.JacobianConfig(spacetime_metric_field,
-        Matrix{Float64}(undef, 4, 4),
-        Vector{Float64}(undef, 4),
-        ForwardDiff.Chunk{4}())
-    return AutoDiffChristoffelCache(spacetime_metric_field = spacetime_metric_field,
-        spacetime_cache = spacetime_cache,
-        cfg = cfg)
-end
-
-function metric_field(spacetime::AbstractSpacetime)
-    (g, position) -> metric!(g, position, spacetime)
-end
-
-function metric_field(spacetime::AbstractSpacetime, cache::AbstractSpacetimeCache)
-    (g, position) -> metric!(g, position, spacetime, cache)
-end
-
-function metric_field(spacetime::AbstractSpacetime, ::Nothing)
-    metric_field(spacetime)
-end
-
-"""
-    metric(position, spacetime)
-
-Evaluate the spacetime metric at the given position and return the result. 
-"""
-function metric(position, spacetime::AbstractSpacetime)
-    g = zeros(4, 4)
-    cache = allocate_cache(spacetime)
-    metric!(g, position, spacetime, cache)
-    return g
-end
-
-"""
-    metric(position, spacetime, cache)
-
-Evaluate the spacetime metric at the given position and return the result using a cache object as temporary storage. 
-"""
-function metric(position, spacetime::AbstractSpacetime, cache)
-    g = zeros(4, 4)
-    metric!(g, position, spacetime, cache)
-    return g
-end
-
-"""
-    volume_element(position, spacetime, g, cache)
-
-Compute the volume element (square root of minus the determinant of the metric) at the given position 
-with a fast determinant for 4x4 symmetric matrices, using `g` and `cache` for temporary storage.
-"""
-function volume_element(position, spacetime::AbstractSpacetime, g, cache)
-    metric!(g, position, spacetime, cache)
-    return sqrt(-det4x4sym(g))
-end
-
-"""
-    volume_element(position, spacetime)
-
-Compute the volume element (square root of minus the determinant of the metric) at the given position 
-with a fast determinant for 4x4 symmetric matrices.
-"""
-function volume_element(position, spacetime::AbstractSpacetime)
-    g = zeros(4, 4)
-    cache = allocate_cache(spacetime)
-    metric!(g, position, spacetime, cache)
-    return sqrt(-det4x4sym(g))
-end
-
-function metric_inverse(position, spacetime::AbstractSpacetime)
-    ginv = zeros(4, 4)
-    g = zeros(4, 4)
-    cache = allocate_cache(spacetime)
-    metric_inverse!(ginv, position, spacetime, g, cache)
-    return ginv
-end
-
-function christoffel(position, spacetime::AbstractSpacetime)
-    Γ = zeros(4, 4, 4)
-    cache = allocate_christoffel_cache(spacetime)
-    christoffel!(Γ, position, spacetime, cache)
-    return Γ
-end
-
-function christoffel(position,
-    spacetime::AbstractSpacetime,
-    cache::AbstractChristoffelCache)
-    Γ = zeros(4, 4, 4)
-    christoffel!(Γ, position, spacetime, cache)
-    return Γ
-end
-
 @inline sign(::ProgradeRotation) = 1.0
 @inline sign(::RetrogradeRotation) = -1.0
 
-""" 
+"""
+    equatorial_ring_areas(edges::AbstractVector, spacetime::AbstractSpacetime)
+
+ 
     equatorial_ring_areas(edges, spacetime)
 
 Approximate areas of the equatorial rings delimited by `edges` in `spacetime`. The spacetime
