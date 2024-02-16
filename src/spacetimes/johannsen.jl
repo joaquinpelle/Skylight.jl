9eed4f0b9529ef0ece63fa415733a49f7447c4b5
joaquinpelle <pelle.joaquin@gmail.com>
2024-02-16 16:38:57 -0300

    Fix typo

--- src/spacetimes/johannsen.jl
+++ src/spacetimes/johannsen.jl
@@ -26,8 +26,8 @@ JohannsenSpacetime(M=1.0, a=0.99, α13=0.5, α22=0.0, α52=0.01, ϵ3=0.0)
     @assert ϵ3>-factor^3 "Invalid deformation parameter ϵ3"
 end
 
-stationarity(::JohannsenKerrSpacetime) = IsStationary()
-axial_symmetry(::JohannsenKerrSpacetime) = IsAxiallySymmetric()
+stationarity(::JohannsenSpacetime) = IsStationary()
+axial_symmetry(::JohannsenSpacetime) = IsAxiallySymmetric()
 
 function event_horizon_radius(spacetime::JohannsenSpacetime)
     spacetime.M * (1 + sqrt(1 - spacetime.a^2))
