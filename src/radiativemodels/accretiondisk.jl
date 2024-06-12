630efa70f22f93f136085fea7990b9589fc74166
joaquinpelle <pelle.joaquin@gmail.com>
2024-06-12 10:13:33 -0300

    Add accretion disk with flat lamppost profile

--- src/radiativemodels/accretiondisk.jl
+++ src/radiativemodels/accretiondisk.jl
@@ -216,6 +216,27 @@ function line_emission_profile(position,
     return model.profile_interpolator(r)
 end
 
+# Accretion disk with tabulated emission profile
+@with_kw struct AccretionDiskWithFlatLamppostProfile{T} <: AbstractAccretionDisk
+    inner_radius::Float64
+    outer_radius::Float64
+    corona_height::Float64
+    rotation_sense::T = ProgradeRotation()
+end
+
+function line_emission_profile(position,
+    momentum,
+    rest_frame_four_velocity,
+    metric,
+    spacetime,
+    model::AccretionDiskWithFlatLamppostProfile,
+    coords_top,
+    cache)
+    r = radius(position, spacetime)
+    h = model.corona_height
+    return h/(r^2+h^2)^1.5
+end
+
 function Eddington_luminosity(M)
     return 4π*PhysicalConstants.c*PhysicalConstants.G*M*PhysicalConstants.mp/PhysicalConstants.sigma_T
 end
