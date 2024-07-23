677a49cdc8fcc2ef1abbeab8fb3b4e583be221c0
joaquinpelle <pelle.joaquin@gmail.com>
2024-07-23 17:36:34 -0300

    Add surface normal to postprocess cache

--- src/postprocess/types.jl
+++ src/postprocess/types.jl
@@ -3,8 +3,10 @@ abstract type AbstractPostProcessCache end
 @with_kw mutable struct ImagePlanePostProcessCache{S, M} <: AbstractPostProcessCache
     observer_metric::Matrix{Float64} = zeros(4, 4)
     emitter_metric::Matrix{Float64} = zeros(4, 4)
+    emitter_metric_inverse::Matrix{Float64} = zeros(4, 4)
     observer_four_velocity::Vector{Float64} = zeros(4)
     rest_frame_four_velocity::Vector{Float64} = zeros(4)
+    surface_normal::Vector{Float64} = zeros(4)
     spacetime_cache::S
     model_cache::M
 end
@@ -12,8 +14,10 @@ end
 @with_kw mutable struct PinholeCameraPostProcessCache{S, M} <: AbstractPostProcessCache
     observer_metric::Matrix{Float64} = zeros(4, 4)
     emitter_metric::Matrix{Float64} = zeros(4, 4)
+    emitter_metric_inverse::Matrix{Float64} = zeros(4, 4)
     observer_four_velocity::Vector{Float64} = zeros(4)
     rest_frame_four_velocity::Vector{Float64} = zeros(4)
+    surface_normal::Vector{Float64} = zeros(4)
     flux_direction::Vector{Float64} = zeros(4)
     spacetime_cache::S
     model_cache::M
